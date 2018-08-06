26efb9cbdd212d3ec749f5e93ac08ddb3bea5bd8
Julian Fleischer <tirednesscankill@warhog.net>
2018-08-06 16:34:08 +0200

    fix whitespace

--- .travis/test_04_install.sh
+++ .travis/test_04_install.sh
@@ -1,7 +1,7 @@
 
 
 if [ -n "$DPKG_ADD_ARCH" ]; then
-  sudo dpkg --add-architecture "$DPKG_ADD_ARCH" 
+  sudo dpkg --add-architecture "$DPKG_ADD_ARCH"
 fi
 
 if [ -n "$PACKAGES" ]; then
@@ -11,4 +11,3 @@ fi
 if [ -n "$PACKAGES" ]; then
   travis_retry sudo apt-get install --no-install-recommends --no-upgrade -qq $PACKAGES
 fi
-
