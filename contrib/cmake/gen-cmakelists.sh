323204147634e40a8f312500618bba9db9db1379
Julian Fleischer <scravy@users.noreply.github.com>
2018-08-07 15:19:58 +0200

    script to generate basic CMakeLists.txt (#26)
    * script to generate basic CMakeLists.txt
    
    * add openssl on macos
    
    * qt libraries if installed via brew
    
    * require qt packagesg
    
    * Added README
    
    * added src/ as include_directories entry

---
+++ contrib/cmake/gen-cmakelists.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )/../.."
+
+(
+echo "cmake_minimum_required(VERSION 3.12)"
+echo "project(unit_e)"
+echo ""
+echo "set(CMAKE_CXX_STANDARD 11)"
+echo ""
+echo "add_definitions(-DHAVE_CONFIG_H)"
+echo ""
+
+echo "include_directories(/usr/local/opt/openssl/include)"
+echo "link_directories(/usr/local/opt/openssl/lib)"
+echo ""
+
+QT_PATH=$(brew info qt 2>/dev/null | head -n4 | tail -n1 | cut -f1 -d' ')
+echo "set(CMAKE_PREFIX_PATH $QT_PATH/lib/cmake)"
+echo ""
+echo "find_package(Qt5Core REQUIRED)"
+echo "find_package(Qt5Widgets REQUIRED)"
+echo "find_package(Qt5Quick REQUIRED)"
+echo ""
+
+echo "include_directories(src)"
+find -E src -type d -regex '.+/[^/.]+' | awk '{ print "include_directories(" $0 ")" }'
+
+echo ""
+
+echo "add_executable(unit_e"
+find -E src -type f -regex '.+\.[ch](pp)?' | awk '{ print "      " $0 }'
+echo ")"
+) > CMakeLists.txt
+
