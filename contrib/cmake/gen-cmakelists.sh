9441ef4d2e127b144ed816201217b7d61414d571
Julian Fleischer <tirednesscankill@warhog.net>
2018-08-07 13:53:08 +0200

    script to generate basic CMakeLists.txt

---
+++ contrib/cmake/gen-cmakelists.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )/../.."
+
+(
+echo "cmake_minimum_required(VERSION 3.12)"
+echo "project(unit_e)"
+echo ""
+echo "set(CMAKE_CXX_STANDARD 11)"
+echo ""
+echo "add_definitions(-DHAVE_CONFIG_H)"
+echo ""
+
+find -E src -type d -regex '.+/[^/.]+' | awk '{ print "include_directories(" $0 ")" }'
+
+echo ""
+
+echo "add_executable(unit_e"
+find -E src -type f -regex '.+\.[ch](pp)?' | awk '{ print "      " $0 }'
+echo ")"
+) > CMakeLists.txt
+
