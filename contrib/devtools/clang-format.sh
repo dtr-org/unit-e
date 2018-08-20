e5d7f309109fa68ff1a72671a12e477cecade03c
Kostiantyn Stepaniuk <kostya.stepanyuk@gmail.com>
2018-08-20 12:09:43 +0200

    Ignore src/crypto folder

--- contrib/devtools/clang-format.sh
+++ contrib/devtools/clang-format.sh
@@ -10,7 +10,7 @@
 export LC_ALL=C.UTF-8
 
 for file in $(git ls-files '*.h'); do
-  if [[ "$file" =~ src/crypto/ctaes/.+ || "$file" =~ src/secp256k1/.+ || "$file" =~ src/univalue/.+ || "$file" =~ src/leveldb/.+ ]]; then
+  if [[ "$file" =~ src/crypto/.+ || "$file" =~ src/secp256k1/.+ || "$file" =~ src/univalue/.+ || "$file" =~ src/leveldb/.+ ]]; then
     echo "Excluding $file"
   else
     echo "Formatting $file..."
@@ -19,7 +19,7 @@ for file in $(git ls-files '*.h'); do
 done
 
 for file in $(git ls-files '*.cpp'); do
-  if [[ "$file" =~ src/crypto/ctaes/.+ || "$file" =~ src/secp256k1/.+ || "$file" =~ src/univalue/.+ || "$file" =~ src/leveldb/.+ ]]; then
+  if [[ "$file" =~ src/crypto/.+ || "$file" =~ src/secp256k1/.+ || "$file" =~ src/univalue/.+ || "$file" =~ src/leveldb/.+ ]]; then
     echo "Excluding $file"
   else
     echo "Formatting $file..."
