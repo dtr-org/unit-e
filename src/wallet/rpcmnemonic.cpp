4ed24266dacf75a134496ae1de3db52296fae755
Julian Fleischer <scravy@users.noreply.github.com>
2018-08-27 19:15:42 +0200

    add mnemonic new (#42)

--- src/wallet/rpcmnemonic.cpp
+++ src/wallet/rpcmnemonic.cpp
@@ -1,8 +1,11 @@
+// Copyright (c) 2015 The ShadowCoin developers
+// Copyright (c) 2017 The Particl developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <base58.h>
 #include <key/mnemonic/mnemonic.h>
+#include <random.h>
 #include <rpc/server.h>
 #include <rpc/util.h>
 #include <util.h>
@@ -17,34 +20,60 @@
 #include <ctime>
 #include <functional>
 
-UniValue mnemonicinfo(const JSONRPCRequest& request) {
-  std::string mnemonic = "";
-  if (request.params.size() > 0) {
-    mnemonic = request.params[0].get_str();
-  }
-  std::string passphrase = "";
-  if (request.params.size() > 1) {
+UniValue mnemonicnew(const JSONRPCRequest &request) {
+  key::mnemonic::Language language = key::mnemonic::Language::ENGLISH;
+  size_t maxTries = 16;
+  size_t numEntropyBytes = 32;
+  std::string passphrase;
+
+  if (request.params.size() >= 2) {
     passphrase = request.params[1].get_str();
   }
-  if (request.fHelp || request.params.size() > 2) {
-    throw std::runtime_error(
-        "mnemonicinfo\n"
-        "\nShow information a BIP39 mnemonic seed "
-        "according to BIP39."
-        "\nShows the mnemonics language, the seed in hex, and the root key as "
-        "base58 private key.\n"
-        "\nArguments:\n"
-        "1. \"seed\"       (string, required) a list of words to create the "
-        "master key from\n"
-        "2. \"passphrase\" (string, optional) an optional passphrase to "
-        "protect the key\n"
-        "\nExamples:\n" +
-        HelpExampleCli("mnemonicinfo",
-                       "\"next debate force grief bleak want truck prepare "
-                       "theme lecture wear century rich grace someone\"") +
-        HelpExampleRpc("mnemonicinfo",
-                       "\"next debate force grief bleak want truck prepare "
-                       "theme lecture wear century rich grace someone\""));
+
+  std::string error;
+  std::vector<uint8_t> entropy(numEntropyBytes);
+  std::vector<uint8_t> seed;
+
+  std::string mnemonic;
+  CExtKey masterKey;
+
+  for (size_t i = 0; i < maxTries; ++i) {
+
+    GetStrongRandBytes(&entropy[0], static_cast<int>(numEntropyBytes));
+
+    if (0 != key::mnemonic::Encode(language, entropy, mnemonic, error)) {
+      throw JSONRPCError(
+          RPC_INTERNAL_ERROR,
+          strprintf("failed to decode mnemonic: %s", error.c_str()));
+    }
+    if (0 != key::mnemonic::ToSeed(mnemonic, passphrase, seed)) {
+      throw JSONRPCError(RPC_INTERNAL_ERROR,
+                         strprintf("failed to calculate seed from mnemonic %s",
+                                   error.c_str()));
+    }
+    masterKey.SetMaster(&seed[0], static_cast<unsigned int>(seed.size()));
+    if (masterKey.key.IsValid()) {
+      break;
+    }
+  }
+  const key::mnemonic::Seed seedInfo(mnemonic, passphrase);
+  UniValue response(UniValue::VOBJ);
+  response.pushKV("mnemonic", UniValue(mnemonic));
+  response.pushKV("master", UniValue(seedInfo.GetExtKey58().ToString()));
+  response.pushKV("entropy", UniValue(seedInfo.GetHexEntropy()));
+  return response;
+}
+
+UniValue mnemonicinfo(const JSONRPCRequest &request) {
+  std::string mnemonic = "";
+  if (request.params.size() > 1) {
+    mnemonic = request.params[1].get_str();
+  } else {
+    throw std::runtime_error("missing required first argument <mnemonic>");
+  }
+  std::string passphrase = "";
+  if (request.params.size() > 2) {
+    passphrase = request.params[2].get_str();
   }
   key::mnemonic::Seed seed(mnemonic, passphrase);
   UniValue response(UniValue::VOBJ);
@@ -56,7 +85,7 @@ UniValue mnemonicinfo(const JSONRPCRequest& request) {
   return response;
 }
 
-UniValue mnemoniclistlanguages(const JSONRPCRequest& request) {
+UniValue mnemoniclistlanguages(const JSONRPCRequest &request) {
   UniValue response(UniValue::VOBJ);
   for (size_t ix = 0; ix < static_cast<int>(key::mnemonic::Language::COUNT);
        ++ix) {
@@ -67,25 +96,58 @@ UniValue mnemoniclistlanguages(const JSONRPCRequest& request) {
   return response;
 }
 
-UniValue importmasterkey(const JSONRPCRequest& request) {
-  if (request.fHelp || request.params.size() > 2 || request.params.size() < 1) {
-    throw std::runtime_error(
-        "importmasterkey\n"
-        "\nImport a master key from a BIP39 seed, with an optional passphrase."
-        "\nArguments:\n"
-        "1. \"seed\"       (string, required) a list of words to create the "
-        "master key from\n"
-        "2. \"passphrase\" (string, optional) an optional passphrase to "
-        "protect the key\n"
-        "\nExamples:\n" +
-        HelpExampleCli("importmasterkey",
-                       "\"next debate force grief bleak want truck prepare "
-                       "theme lecture wear century rich grace someone\"") +
-        HelpExampleRpc("importmasterkey",
-                       "\"next debate force grief bleak want truck prepare "
-                       "theme lecture wear century rich grace someone\""));
+UniValue mnemonic(const JSONRPCRequest &request) {
+  static const std::string help =
+      ""
+      "mnemonic new|decode|addchecksum|dumpwords|listlanguages\n"
+      "mnemonic new [password]\n"
+      "    Generate a new mnemonic seed for setting a master\n"
+      "    key for the hierarchical deterministic wallet.\n"
+      "mnemonic info <mnemonic> [password]\n"
+      "    Shows various kinds of information about a mnemonic seed:\n"
+      "    \"language\": the language detected from the words,\n"
+      "    \"bip39_seed\": the seed decoded and converted to hex,\n"
+      "    \"bip32_root\": the private key derived from this seed,\n"
+      "    \"entropy\": the entropy contained in this seed.\n"
+      "mnemonic listlanguages\n"
+      "    Print list of supported languages.\n"
+      "\n";
+  if (request.fHelp || request.params.size() == 0) {
+    throw std::runtime_error(help);
   }
-  CWallet* const wallet = GetWalletForJSONRPCRequest(request);
+  std::string subcommand = request.params[0].get_str();
+
+  if (subcommand == "new") {
+    return mnemonicnew(request);
+  } else if (subcommand == "info") {
+    return mnemonicinfo(request);
+  } else if (subcommand == "listlanguages") {
+    return mnemoniclistlanguages(request);
+  } else {
+    throw std::runtime_error("unknown mnemonic subcommand: " + subcommand);
+  }
+}
+
+UniValue importmasterkey(const JSONRPCRequest &request) {
+  static const std::string help =
+      "importmasterkey\n"
+      "\nImport a master key from a BIP39 seed, with an optional passphrase."
+      "\nArguments:\n"
+      "1. \"seed\"       (string, required) a list of words to create the "
+      "master key from\n"
+      "2. \"passphrase\" (string, optional) an optional passphrase to "
+      "protect the key\n"
+      "\nExamples:\n" +
+      HelpExampleCli("importmasterkey",
+                     "\"next debate force grief bleak want truck prepare "
+                     "theme lecture wear century rich grace someone\"") +
+      HelpExampleRpc("importmasterkey",
+                     "\"next debate force grief bleak want truck prepare "
+                     "theme lecture wear century rich grace someone\"");
+  if (request.fHelp || request.params.size() > 2 || request.params.size() < 1) {
+    throw std::runtime_error(help);
+  }
+  CWallet *const wallet = GetWalletForJSONRPCRequest(request);
   if (!EnsureWalletIsAvailable(wallet, request.fHelp)) {
     throw std::runtime_error("no unlocked wallet open!");
   }
@@ -117,14 +179,14 @@ UniValue importmasterkey(const JSONRPCRequest& request) {
  * Dumps all the reserve keys for verifying the mnemonic seed generates the same
  * wallet deterministically.
  */
-UniValue listreservekeys(const JSONRPCRequest& request) {
-  CWallet* const wallet = GetWalletForJSONRPCRequest(request);
+UniValue listreservekeys(const JSONRPCRequest &request) {
+  CWallet *const wallet = GetWalletForJSONRPCRequest(request);
   if (!EnsureWalletIsAvailable(wallet, request.fHelp)) {
     throw std::runtime_error("no unlocked wallet open!");
   }
   const std::map<CKeyID, int64_t> allReserveKeys = wallet->GetAllReserveKeys();
   UniValue reserveKeys(UniValue::VARR);
-  for (auto& it : allReserveKeys) {
+  for (auto &it : allReserveKeys) {
     const CKeyID keyID = it.first;
     CKey key;
     wallet->GetKey(keyID, key);
@@ -141,17 +203,16 @@ UniValue listreservekeys(const JSONRPCRequest& request) {
 }
 
 // clang-format off
-static const CRPCCommand commands[] =
-{ //  category               name                      actor (function)         argNames
-  //  ---------------------  ------------------------  -----------------------  ----------
-  {   "mnemonic",            "mnemonicinfo",           &mnemonicinfo,           {"seed", "passphrase"} },
-  {   "mnemonic",            "mnemoniclistlanguages",  &mnemoniclistlanguages,  {} },
-  {   "hidden",              "listreservekeys",        &listreservekeys,        {} },
-  {   "wallet",              "importmasterkey",        &importmasterkey,        {"seed", "passphrase"} },
+static const CRPCCommand commands[] = {
+//  category               name                      actor (function)         argNames
+//  ---------------------  ------------------------  -----------------------  ------------------------------------------
+    { "mnemonic",          "mnemonic",               &mnemonic,               {"subcommand", "mnemonic", "passphrase"}},
+    { "hidden",            "listreservekeys",        &listreservekeys,        {}},
+    { "wallet",            "importmasterkey",        &importmasterkey,        {"seed", "passphrase"}},
 };
 // clang-format on
 
-void RegisterMnemonicRPCCommands(CRPCTable& t) {
+void RegisterMnemonicRPCCommands(CRPCTable &t) {
   for (unsigned int vcidx = 0; vcidx < ARRAYLEN(commands); vcidx++) {
     t.appendCommand(commands[vcidx].name, &commands[vcidx]);
   }
