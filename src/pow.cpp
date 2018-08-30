a0b814b1502b9e9f6acae06e668a62a20fad51b5
Matteo Sumberaz <matteo@thirdhash.com>
2018-08-30 16:56:45 +0200

    Fix tests

--- src/pow.cpp
+++ src/pow.cpp
@@ -75,7 +75,9 @@ unsigned int CalculateNextWorkRequired(const CBlockIndex* pindexLast, int64_t nF
 bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params& params)
 {
     // UNIT-E: If the hash is the genesis then make it pass anyway
-    if(hash.GetHex() == "5b6d9c77a0c7bd98341bfeb45711851e45c6ea85cfca825f61bd1d0b967a7b06") {return true;}
+    if(hash.GetHex() == "5b6d9c77a0c7bd98341bfeb45711851e45c6ea85cfca825f61bd1d0b967a7b06") {
+      return true;
+    }
 
     bool fNegative;
     bool fOverflow;
