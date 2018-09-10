20d1577e23efe2b88f74bf1fd8d81b92de60243e
Matteo Sumberaz <matteo@thirdhash.com>
2018-09-10 11:23:02 +0200

    Add vote to runner

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -67,6 +67,7 @@ BASE_SCRIPTS= [
     'wallet_accounts.py',
     'p2p_segwit.py',
     'wallet_dump.py',
+    'esperanza_vote.py',
     'rpc_listtransactions.py',
     # vv Tests less than 60s vv
     'p2p_sendheaders.py',
