ac684fb2c7945a309f14b7cffa2f0887caca53ce
Julian Fleischer <julian@thirdhash.com>
2018-09-12 12:36:42 +0200

    add SyncStatus enum

---
+++ src/sync_status.h
@@ -0,0 +1,24 @@
+// Copyright (c) 2018 The unit-e core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+
+#ifndef UNIT_E_STATUS_H
+#define UNIT_E_STATUS_H
+
+#include <better-enums/enum.h>
+
+// clang-format off
+BETTER_ENUM(
+  SyncStatus,
+  uint8_t,
+  SYNCED,
+  IMPORTING,
+  REINDEXING,
+  NO_TIP,
+  MINIMUM_CHAIN_WORK_NOT_REACHED,
+  MAX_TIP_AGE_EXCEEDED
+)
+// clang-format on
+
+#endif //UNIT_E_STATUS_H
