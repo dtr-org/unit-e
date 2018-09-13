45bef3dcd26b1ce7a69f9a047337f228b620d9fa
Matteo Sumberaz <matteo@thirdhash.com>
2018-09-13 16:36:58 +0200

    Add lint step for python shebang

---
+++ contrib/devtools/lint-python-shebang.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+export LC_ALL=C
+
+cd $(git rev-parse --show-toplevel)/test/functional
+exclusions="test_framework.*"
+
+files=$(git ls-files | grep -E ".*\.py" | grep -v -E ${exclusions})
+
+RET=0
+for file in ${files}
+do
+    line=$(head -n 1 ${file})
+    if [[ ${line} != "#!/usr/bin/env python3" ]]; then
+        echo "Missing python shebang in ${file}"
+        RET=1;
+    fi
+done
+
+exit ${RET}
