111b8d68e49d94c3807d5bedff690662323b0c57
Julian Fleischer <julian@thirdhash.com>
2018-09-19 11:30:05 +0200

    skeleton for proposer tests

--- test/functional/proposer_propose_blocks.py
+++ test/functional/proposer_propose_blocks.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
-# Copyright (c) 2014-2017 The unit-e developers
+# Copyright (c) 2018 The unit-e developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 from test_framework.util import *
 from test_framework.test_framework import UnitETestFramework
-import time
 
 class EsperanzaTest(UnitETestFramework):
 
     def set_test_params(self):
-        self.num_nodes = 6
+
+        self.keys = [
+            'swap fog boost power mountain pair gallery crush price fiscal thing supreme chimney drastic grab acquire any cube cereal another jump what drastic ready',
+            'chef gas expect never jump rebel huge rabbit venue nature dwarf pact below surprise foam magnet science sister shrimp blanket example okay office ugly',
+            'narrow horror cheap tape language turn smart arch grow tired crazy squirrel sun pumpkin much panic scissors math pass tribe limb myself bone hat',
+            'soon empty next roof proof scorpion treat bar try noble denial army shoulder foam doctor right shiver reunion hub horror push theme language fade'
+        ]
+
+        self.num_nodes = len(self.keys)
 
         self.extra_args = list([
             '-proposing=1',
@@ -20,62 +27,26 @@ class EsperanzaTest(UnitETestFramework):
 
     def run_test(self):
 
-        keys = [
-            'swap fog boost power mountain pair gallery crush price fiscal thing supreme chimney drastic grab acquire any cube cereal another jump what drastic ready',
-            'chef gas expect never jump rebel huge rabbit venue nature dwarf pact below surprise foam magnet science sister shrimp blanket example okay office ugly',
-            'narrow horror cheap tape language turn smart arch grow tired crazy squirrel sun pumpkin much panic scissors math pass tribe limb myself bone hat',
-            'soon empty next roof proof scorpion treat bar try noble denial army shoulder foam doctor right shiver reunion hub horror push theme language fade'
-        ]
-
-        nodes = self.nodes
-
-        # at first none of the nodes will propose as it has no peers
+        # import master keys which unlock funds from the genesis blocks
         for i in range(0, self.num_nodes):
-            status = nodes[i].proposerstatus()
-            wallet = status['wallets'][0]
-            assert_equal(wallet['balance'], Decimal('0.00000000'))
-            assert_equal(wallet['stakeable_balance'], Decimal('0.00000000'))
-            assert_equal(wallet['status'], 'NOT_PROPOSING_NO_PEERS')
+            self.nodes[i].importmasterkey(self.keys[i])
 
         # connect all nodes with each other
         for i in range(0, self.num_nodes):
             for j in range(i+1, self.num_nodes):
-                connect_nodes_bi(nodes, i, j)
+                connect_nodes_bi(self.nodes, i, j)
 
         # wakes all the proposers in case they are sleeping right now
         for i in range(0, self.num_nodes):
-            nodes[i].proposerwake()
-
-        # none of the nodes has any money now, but a bunch of friends
-        for i in range(0, self.num_nodes):
-            status = nodes[i].proposerstatus()
-            assert_equal(status['incoming_connections'], self.num_nodes - 1)
-            assert_equal(status['outgoing_connections'], self.num_nodes - 1)
-            wallet = status['wallets'][0]
-            assert_equal(wallet['status'], 'NOT_PROPOSING_NOT_ENOUGH_BALANCE')
-
-        # import master keys which unlock funds from the genesis blocks
-        for i in range(0, len(keys)):
-            nodes[i].importmasterkey(keys[i])
-
-        # wakes all the proposers in case they are sleeping right now
-        for i in range(0, self.num_nodes):
-            nodes[i].proposerwake()
+            self.nodes[i].proposerwake()
 
         # now the funded nodes should have switched to IS_PROPOSING
-        for i in range(0, len(keys)):
-            status = nodes[i].proposerstatus()
+        for i in range(0, self.num_nodes):
+            status = self.nodes[i].proposerstatus()
             wallet = status['wallets'][0]
             assert_equal(wallet['balance'], Decimal('10000.00000000'))
             assert_equal(wallet['stakeable_balance'], Decimal('10000.00000000'))
             assert_equal(wallet['status'], 'IS_PROPOSING')
-        # and others shoulds till not have enough funds for proposing blocks
-        for i in range(len(keys), self.num_nodes):
-            status = nodes[i].proposerstatus()
-            wallet = status['wallets'][0]
-            assert_equal(wallet['balance'], Decimal('0.00000000'))
-            assert_equal(wallet['stakeable_balance'], Decimal('0.00000000'))
-            assert_equal(wallet['status'], 'NOT_PROPOSING_NOT_ENOUGH_BALANCE')
 
         print("Test succeeded.")
 
