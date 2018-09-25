0d7b75060de1b53e39a600dba76fd73af35dc8d7
Julian Fleischer <scravy@users.noreply.github.com>
2018-09-25 23:25:16 +0200

    Fix a compiler warning and remove some unused code (#113)
    * remove unused IsProofOfStake function (all block are PoS blocks)
    
    * declare class final to prevent warning about non-virtual destructor

--- src/primitives/block.h
+++ src/primitives/block.h
@@ -89,11 +89,6 @@ public:
         *((CBlockHeader*)this) = header;
     }
 
-    bool IsProofOfStake() const
-    {
-        return (vtx.size() > 0 && vtx[0]->IsCoinStake());
-    }
-
     ADD_SERIALIZE_METHODS;
 
     template <typename Stream, typename Operation>
