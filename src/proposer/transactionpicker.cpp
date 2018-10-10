2f4a2365a76fba55e914f23eca85d4bb6dc00d45
Julian Fleischer <scravy@users.noreply.github.com>
2018-10-10 14:31:32 +0200

    add proposer::TransactionPicker (#153)
    This adds a class named TransactionPicker and introduces a new namespace proposer in its own directory. I sort of got fed up of the untestability of bitcoin and that everything is intertwined with each other directly.
    
    In order to propose a block I need to build one which is I have to (1) pick transactions (2) put a coinstake transaction (includes block height, signature, reward output) (3) sign the thing. Picking transactions is already implemented in bitcoin but it's well buried inside CBlockAssembler which does too many things at once for my taste + deals with that horrendous CBlockTemplate (see comment in BlockAssemblerAdapter in the source).
    
    Since I want my Proposer to be testable and (1+2+3) are essentially simple operations I want the code to be simple. So here is the part which performs 1, by delegating to bitcoin's block assembler (for now). This way I do not have to change the block assembler or overwrite the coinbase transaction in the block template.
    
    I would be interested in hearing your thoughts about my approach, as I intend to follow it with everything I do from now on. Maybe you are fundamentally opposed to virtual methods or whatever.
    
    In bitcoin there have been small efforts to extract things into Interfaces (CValidationInterface for instance) also. More recent work is on separating wallet and node and also here they start to create interfaces (bitcoin/bitcoin#14437, src/interfaces).

---
+++ src/proposer/transactionpicker.cpp
@@ -0,0 +1,84 @@
+// Copyright (c) 2018 The unit-e core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <proposer/transactionpicker.h>
+
+#include <amount.h>
+#include <miner.h>
+#include <script/script.h>
+
+namespace proposer {
+
+//! An adapter to bitcoins CBlockAssembler.
+//!
+//! The CBlockAssembler comprises the logic for picking transactions.
+//! In order to maintain compatibility with bitcoin but not rely on
+//! CBlockTemplate and not change existing code this adapter is used
+//! to just extract the transactions to be included when building a new
+//! block.
+//!
+//! CBlockTemplate is an invention to support external mining software.
+//! Previous iterations of bitcoin had an rpc method called "getwork"
+//! which would only return a block header to solve the hash for. This
+//! effectively took away power from the miners in a mining pool and
+//! centralize the decision which transactions to include in mined blocks
+//! with the pool operator. To combat this BIP22 and BIP23 defined the
+//! "getblocktemplate" rpc to supersede "getwork".
+//!
+//! Since there is no mining in unit-e we do not use the block
+//! templates. The proposer can assemble a block itself, which in turn
+//! greatly reduces complexity of the process to create new blocks and
+//! the amount of code needed to do so.
+//!
+//! The implementation, in fact the existence, of this very class is
+//! hidden from other compilation units since declaration and definition
+//! are here in the *.cpp-file. This is on purpose and inspired by the
+//! pImpl idiom. It should decrease compile times and helps
+//! encapsulation. Since TransactionPicker is an interface which
+//! can be mocked easily this design should greatly help unit testing
+//! components which use a TransactionPicker.
+class BlockAssemblerAdapter final : public TransactionPicker {
+
+ private:
+  const CChainParams &m_chainParams;
+
+ public:
+  explicit BlockAssemblerAdapter(const CChainParams &chainParams)
+      : m_chainParams(chainParams) {}
+
+  ~BlockAssemblerAdapter() override = default;
+
+  PickTransactionsResult PickTransactions(
+      const PickTransactionsParameters &parameters) override {
+
+    ::BlockAssembler::Options blockAssemblerOptions;
+    blockAssemblerOptions.blockMinFeeRate = parameters.m_minFees;
+    blockAssemblerOptions.nBlockMaxWeight = parameters.m_maxWeight;
+
+    ::BlockAssembler blockAssembler(m_chainParams, blockAssemblerOptions);
+
+    // The block assembler unfortunately also creates a bitcoin-style
+    // coinbase transaction. We do not want to touch that logic to
+    // retain compatibility with bitcoin. The construction of the
+    // coinstake transaction is left to the component using a
+    // TransactionPicker to build a block. Therefore we just pass an
+    // empty script to the blockAssembler.
+    CScript script(1);
+    script.push_back(OP_RETURN);
+    std::unique_ptr<CBlockTemplate> blockTemplate =
+        blockAssembler.CreateNewBlock(script, /* fMineWitnessTx */ true);
+
+    return PickTransactionsResult{std::move(blockTemplate->block.vtx),
+                                  std::move(blockTemplate->vTxFees)};
+  };
+};
+
+std::unique_ptr<TransactionPicker> TransactionPicker::MakeBlockAssemblerAdapter(
+    const CChainParams &chainParams) {
+
+  return std::unique_ptr<TransactionPicker>(
+      new BlockAssemblerAdapter(chainParams));
+}
+
+}  // namespace proposer
---
+++ src/test/proposer/blockassembleradapter_tests.cpp
@@ -0,0 +1,44 @@
+// Copyright (c) 2018 The unit-e core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <chainparams.h>
+#include <test/test_unite.h>
+#include <validation.h>
+
+#include <string>
+#include <vector>
+
+#include <boost/test/unit_test.hpp>
+
+#include <proposer/transactionpicker.h>
+
+struct RegtestingSetup : public TestingSetup {
+  RegtestingSetup() : TestingSetup(CBaseChainParams::REGTEST) {}
+};
+
+BOOST_FIXTURE_TEST_SUITE(blockassembleradapter_tests, RegtestingSetup)
+
+BOOST_AUTO_TEST_CASE(block_assembler_adapter_test) {
+
+  // this test checks the technical correctness, that is basically
+  // that it does not crash and does yield a value. For a proper test
+  // transactions will have to be mocked.
+
+  std::unique_ptr<proposer::TransactionPicker> blockAssemblerAdapter =
+      proposer::TransactionPicker::MakeBlockAssemblerAdapter(::Params());
+
+  proposer::TransactionPicker::PickTransactionsParameters params;
+
+  auto result = blockAssemblerAdapter->PickTransactions(params);
+
+  CAmount fees = 0;
+  for (const auto fee : result.m_fees) {
+    fees += fee;
+  }
+
+  // do something with the result to prevent it being optimized away
+  BOOST_CHECK_GE(fees, 0);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
