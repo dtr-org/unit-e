d62b54e2b94536725351daa8ceb6dafff59b3560
Julian Fleischer <julian@thirdhash.com>
2018-10-21 23:10:31 +0200

    Initialize proposer using Injector

--- src/proposer/proposer_init.cpp
+++ src/proposer/proposer_init.cpp
@@ -4,11 +4,7 @@
 
 #include <proposer/proposer_init.h>
 
-#include <proposer/blockproposer.h>
-#include <proposer/chainstate.h>
-#include <proposer/network.h>
-#include <proposer/proposer.h>
-#include <proposer/transactionpicker.h>
+#include <injector.h>
 #include <util.h>
 
 #include <mutex>
@@ -17,12 +13,12 @@
 namespace proposer {
 
 static std::mutex initLock;
-static std::unique_ptr<Proposer> proposer = nullptr;
+static std::unique_ptr<UnitEInjector> injector = nullptr;
 
 bool InitProposer(const esperanza::Settings &settings,
                   const std::vector<CWallet *> &wallets) {
   std::unique_lock<decltype(initLock)> lock;
-  if (proposer) {
+  if (injector) {
     return false;
   }
   if (!settings.m_proposing) {
@@ -30,45 +26,36 @@ bool InitProposer(const esperanza::Settings &settings,
              "not starting proposer, proposing is not activated.\n");
     return true;
   }
-  //  try {
-  //    std::shared_ptr<ChainState> chainInterface(
-  //        std::move(ChainState::MakeChain()));
-  //    std::shared_ptr<Network> networkInterface(
-  //        std::move(Network::MakeNetwork()));
-  //    std::shared_ptr<TransactionPicker> transactionPicker(
-  //        std::move(TransactionPicker::MakeBlockAssemblerAdapter(::Params())));
-  //    std::shared_ptr<BlockProposer> blockProposer(std::move(
-  //        BlockProposer::MakeBlockProposer(chainInterface,
-  //        transactionPicker)));
-  //    proposer.reset(new Proposer(settings, wallets, networkInterface,
-  //                                chainInterface, blockProposer));
-  //    return true;
-  //  } catch (const std::runtime_error &exc) {
-  //    LogPrint(BCLog::FINALIZATION, "failed to create proposer threads: %s\n",
-  //             exc.what());
-  //    return false;
-  //  }
+  auto _injector = MakeUnique<UnitEInjector>();
+  injector.swap(_injector);
+  try {
+    injector->Initialize();
+  } catch (const std::runtime_error &exc) {
+    LogPrint(BCLog::FINALIZATION, "failed to create proposer threads: %s\n",
+             exc.what());
+    return false;
+  }
   return true;
 }
 
 void StartProposer() {
-  if (proposer) {
+  if (injector) {
     LogPrint(BCLog::FINALIZATION, "starting proposer threads...\n");
-    proposer->Start();
+    injector->GetProposer()->Start();
   }
 }
 
 void StopProposer() {
-  if (proposer) {
+  if (injector) {
     LogPrint(BCLog::FINALIZATION, "stopping proposer threads...\n");
-    proposer->Stop();
+    injector->GetProposer()->Stop();
     LogPrint(BCLog::FINALIZATION, "all proposer threads exited.\n");
   }
 }
 
 void WakeProposer(const CWallet *wallet) {
-  if (proposer) {
-    proposer->Wake(wallet);
+  if (injector) {
+    injector->GetProposer()->Wake(wallet);
   }
 }
 
