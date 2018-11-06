6823bd452f5e39a3ee66efde4f0228b130c0b2d1
Kostiantyn Stepaniuk <kostya.stepanyuk@gmail.com>
2018-11-06 10:47:20 +0100

    Document ValidateCandidateBlockTx

--- src/snapshot/snapshot_validation.h
+++ src/snapshot/snapshot_validation.h
@@ -11,6 +11,12 @@
 
 namespace snapshot {
 
+//! ValidateCandidateBlockTx validates the single TX of the candidate block
+//! and must be used inside ConnectBlock function
+//!
+//! \param tx which is being validated
+//! \param blockIndex that represents the block which has tx
+//! \param view that contains the snapshot hash of the previous block
 bool ValidateCandidateBlockTx(const CTransaction &tx,
                               const CBlockIndex *blockIndex,
                               const CCoinsViewCache &view);
