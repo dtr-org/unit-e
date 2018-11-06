bb47511b71ce0b8e26dce12bd9f72d872ef2b009
Matteo Sumberaz <matteo@thirdhash.com>
2018-11-06 12:29:27 +0100

    Improve deposit test

--- src/test/esperanza/finalizationstate_deposit_tests.cpp
+++ src/test/esperanza/finalizationstate_deposit_tests.cpp
@@ -30,14 +30,21 @@ BOOST_AUTO_TEST_CASE(process_deposit_tx) {
 
   FinalizationStateSpy spy;
   uint256 validatorIndex = GetRandHash();
+  uint256 validatorIndex2 = GetRandHash();
   CAmount depositSize = spy.MinDepositSize();
 
   BOOST_CHECK_EQUAL(spy.ValidateDeposit(validatorIndex, depositSize),
                     +Result::SUCCESS);
+  BOOST_CHECK_EQUAL(spy.ValidateDeposit(validatorIndex2, depositSize),
+                    +Result::SUCCESS);
   spy.ProcessDeposit(validatorIndex, depositSize);
+  spy.ProcessDeposit(validatorIndex2, depositSize);
 
   std::map<uint256, Validator> validators = spy.Validators();
-  auto it = validators.find(validatorIndex);
+  auto it = validators.find(validatorIndex2);
+  BOOST_CHECK(it != validators.end());
+
+  it = validators.find(validatorIndex);
   BOOST_CHECK(it != validators.end());
 
   Validator validator = it->second;
