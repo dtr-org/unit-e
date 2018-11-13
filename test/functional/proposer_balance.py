2108f2660e454990ba00c491304ca10faff74516
Andres Correa Casablanca <andres@thirdhash.com>
2018-11-13 16:10:09 +0100

    Fix fees handling in proposer balance test
    Signed-off-by: Andres Correa Casablanca <andres@thirdhash.com>

--- test/functional/proposer_balance.py
+++ test/functional/proposer_balance.py
@@ -6,20 +6,24 @@
 """Test Blockchain's rewards & UTXOs balance consistency"""
 
 from random import (
+    getstate as rnd_getstate,
     randint,
     seed,
     setstate as rnd_setstate,
-    getstate as rnd_getstate
 )
 
 from decimal import Decimal
 
 from test_framework.authproxy import JSONRPCException
 from test_framework.test_framework import UnitETestFramework
-from test_framework.util import sync_blocks, assert_equal
+from test_framework.util import (
+    assert_equal,
+    sync_blocks,
+    sync_mempools
+)
 
 
-class BlockchainBalanceTest(UnitETestFramework):
+class ProposerBalanceTest(UnitETestFramework):
     def set_test_params(self):
         self.num_nodes = 3
         self.extra_args = [
@@ -47,12 +51,13 @@ class BlockchainBalanceTest(UnitETestFramework):
         # We start our tests with an initial amount of created money
         created_money = nodes[0].gettxoutsetinfo()['total_amount']
 
-        # We'll create a certain amount of money on the next text, so we update
+        # First: Checking without transactions, votes nor slashing
         created_money = self.__test_empty_blocks_balance(created_money, nodes)
+
+        # Second: Checking with transactions & fees
         self.__test_transaction_blocks_balance(created_money, nodes)
 
     def __test_empty_blocks_balance(self, created_money, nodes):
-        # First: Checking without transactions, votes nor slashing
         for i in range(300):
             node_idx = i % 3
 
@@ -62,7 +67,6 @@ class BlockchainBalanceTest(UnitETestFramework):
             coinstake_tx_id = block_info['tx'][0]
             coinstake_tx_info = nodes[node_idx].gettransaction(coinstake_tx_id)
 
-            # This assertion is very simplistic (it does not consider fees!)
             created_money += coinstake_tx_info['details'][0]['amount']
             assert_equal(
                 created_money,
@@ -76,30 +80,41 @@ class BlockchainBalanceTest(UnitETestFramework):
         node0_address = nodes[0].getaccountaddress('')
         node1_address = nodes[1].getaccountaddress('')
         node2_address = nodes[2].getaccountaddress('')
-        rnd_state = rnd_getstate()
+
+        rnd_state = rnd_getstate()  # We do this to isolate this test
         seed(3141592)
-        # Second: Checking with transactions
-        for i in range(300):
+
+        for i in range(15):
             node_idx = i % 3
 
-            self.__execute_random_transactions(
+            # We keep track of each transaction's "origin" in order to make
+            # easier testing (gettransaction only works if the transaction was
+            # created by the node's wallet).
+            tx_map = self.__execute_random_transactions(
                 node0_address, node1_address, node2_address, nodes
             )
 
             self.__generate_block(nodes, node_idx)
             block_info = self.__get_last_block_info(node_idx, nodes)
 
-            block_tx = block_info['tx']
-            coinstake_tx_id = block_tx[0]
-            coinstake_tx_info = nodes[node_idx].gettransaction(coinstake_tx_id)
+            transactions = [
+                # We fallback to node_idx for the 0th transaction
+                nodes[tx_map.get(tx_id, node_idx)].gettransaction(tx_id)
+                for tx_id in block_info['tx']
+            ]
+
+            coinstake_tx_info = transactions[0]
 
             created_money += coinstake_tx_info['details'][0]['amount']
+            for tx in transactions[1:]:
+                created_money += tx['fee']  # Fee is expressed as negative
 
             # This should fail now
             assert_equal(
                 created_money,
                 nodes[node_idx].gettxoutsetinfo()['total_amount']
             )
+
         rnd_setstate(rnd_state)
 
     def __get_last_block_info(self, node_idx, nodes):
@@ -109,19 +124,36 @@ class BlockchainBalanceTest(UnitETestFramework):
     def __execute_random_transactions(
             self, node0_address, node1_address, node2_address, nodes
     ):
+        def random_fee():
+            return Decimal(randint(10000, 19999)) / 100000000
+
+        def random_amount():
+            return Decimal(randint(100, 199)) / 100
+
         # The transactions themselves are not very important, but we want to
         # ensure that is hard to manually adjust the test to make it pass
         # WARNING: Decimal objects with "too much" precision will cause errors
-        nodes[0].settxfee(Decimal(randint(10000, 19999)) / 100000000)
-        nodes[1].settxfee(Decimal(randint(10000, 19999)) / 100000000)
-        nodes[2].settxfee(Decimal(randint(10000, 19999)) / 100000000)
+        nodes[0].settxfee(random_fee())
+        nodes[1].settxfee(random_fee())
+        nodes[2].settxfee(random_fee())
 
-        nodes[0].sendtoaddress(node1_address, Decimal(randint(100, 199)) / 100)
-        nodes[0].sendtoaddress(node2_address, Decimal(randint(100, 199)) / 100)
-        nodes[1].sendtoaddress(node0_address, Decimal(randint(100, 199)) / 100)
-        nodes[1].sendtoaddress(node2_address, Decimal(randint(100, 199)) / 100)
-        nodes[2].sendtoaddress(node0_address, Decimal(randint(100, 199)) / 100)
-        nodes[2].sendtoaddress(node1_address, Decimal(randint(100, 199)) / 100)
+        tx_ids = [
+            nodes[0].sendtoaddress(node1_address, random_amount()),
+            nodes[0].sendtoaddress(node2_address, random_amount()),
+            nodes[1].sendtoaddress(node0_address, random_amount()),
+            nodes[1].sendtoaddress(node2_address, random_amount()),
+            nodes[2].sendtoaddress(node0_address, random_amount()),
+            nodes[2].sendtoaddress(node1_address, random_amount()),
+        ]
+
+        sync_mempools(nodes)
+
+        # We return this mapping just for convenience, it's not really important
+        return {
+            tx_ids[0]: 0, tx_ids[1]: 0,
+            tx_ids[2]: 1, tx_ids[3]: 1,
+            tx_ids[4]: 2, tx_ids[5]: 2
+        }
 
     def __load_wallets(self, nodes):
         nodes[0].importmasterkey(
@@ -155,4 +187,4 @@ class BlockchainBalanceTest(UnitETestFramework):
 
 
 if __name__ == '__main__':
-    BlockchainBalanceTest().main()
+    ProposerBalanceTest().main()
