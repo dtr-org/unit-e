7800047eb3c60262c7e59bfab0c44b29b1f49ddd
Cornelius Schumacher <cornelius@thirdhash.com>
2018-11-19 17:24:38 +0100

    Use consistent format for Unit-e copyright headers
    Always use the form "Copyright (c) <year> The Unit-e developers"
    
    Signed-off-by: Cornelius Schumacher <cornelius@thirdhash.com>

--- test/functional/proposer_balance.py
+++ test/functional/proposer_balance.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (c) 2018 The unit-e core developers
+# Copyright (c) 2018 The Unit-e developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Test Blockchain's rewards & UTXOs balance consistency"""
