a9400fde08cb751f52f1cc6d55fa20205155e88d
Matteo Sumberaz <matteo@thirdhash.com>
2018-11-21 12:03:18 +0100

    Remove duplicate import
    Signed-off-by: Matteo Sumberaz <matteo@thirdhash.com>

--- src/esperanza/vote.cpp
+++ src/esperanza/vote.cpp
@@ -2,7 +2,6 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "vote.h"
 #include <esperanza/vote.h>
 #include <key.h>
 #include <keystore.h>
