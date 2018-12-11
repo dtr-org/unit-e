79ce0c969483499206ead4a6a9247e5faef77597
Stanislav Frolov <stanislav@thirdhash.com>
2018-12-11 10:37:30 +0100

    Add finalization_commits test to test_runner
    Signed-off-by: Stanislav Frolov <stanislav@thirdhash.com>

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -151,6 +151,7 @@ BASE_SCRIPTS= [
     'wallet_mnemonicnew.py',
     'wallet_importmasterkey.py',
     'esperanza_admin_validation.py',
+    'finalization_commits.py'
     'proposer_multiwallet.py',
     'proposer_stakeable_balance.py',
     'proposer_settings.py',
