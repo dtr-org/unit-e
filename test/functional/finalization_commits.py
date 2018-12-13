31513e8da668e5dab7e993e52e079e6db1c67d81
Stanislav Frolov <stanislav@thirdhash.com>
2018-12-13 13:07:49 +0100

    Add test case for most recent block find
    Signed-off-by: Stanislav Frolov <stanislav@thirdhash.com>

--- test/functional/finalization_commits.py
+++ test/functional/finalization_commits.py
@@ -38,7 +38,7 @@ class GetCommitsTest(UnitETestFramework):
 
     def set_test_params(self):
         self.num_nodes = 1
-        self.extra_args = [[ '-printtoconsole', '-debug=all', '-whitelist=127.0.0.1', '-esperanzaconfig={"epochLength": 5}' ]];
+        self.extra_args = [[ '-printtoconsole', '-debug=all', '-whitelist=127.0.0.1', '-esperanzaconfig={"epochLength": 5}' ]]
         self.setup_clean_chain = True
 
     def setup_network(self):
@@ -117,8 +117,11 @@ class GetCommitsTest(UnitETestFramework):
         self.check_commits(0, self.blocks[10:15])
 
         self.getcommits([self.blocks[14]]) #expect [15]
-        self.check_commits(1, self.blocks[15:])
+        self.check_commits(1, [self.blocks[15]])
 
+        # Ask for unknown block hash, check most recent block is 14.
+        self.getcommits([self.blocks[9], self.blocks[14], 0x4242424242]) # expect [15]
+        self.check_commits(1, [self.blocks[15]])
 
 class CommitsTest(UnitETestFramework):
     def __init__(self):
