387fead49a44b9fbbaac801975b320cca4629e5a
Julian Fleischer <julian@thirdhash.com>
2018-12-17 16:00:34 +0100

    Add stubbing for proposer and simple test whether it runs or not
    Signed-off-by: Julian Fleischer <julian@thirdhash.com>

--- src/test/proposer/proposer_tests.cpp
+++ src/test/proposer/proposer_tests.cpp
@@ -18,12 +18,64 @@
 
 #include <test/fakeit/fakeit.hpp>
 
+namespace {
+
+struct Fixture {
+
+  using Proposer = proposer::Proposer;
+
+  ArgsManager args_manager;
+
+  Settings settings;
+  blockchain::Parameters parameters = blockchain::Parameters::MainNet();
+  std::unique_ptr<blockchain::Behavior> behavior = blockchain::Behavior::New(&args_manager);
+
+  struct MultiWalletMock : public proposer::MultiWallet {
+    std::vector<CWallet *> wallets;
+    const std::vector<CWallet *> &GetWallets() const override {
+      return wallets;
+    };
+  };
+
+  CWallet wallet;
+  MultiWalletMock multi_wallet_mock;
+
+  Fixture()
+      : wallet([&] {
+          esperanza::WalletExtensionDeps deps;
+          deps.settings = &settings;
+          return deps;
+        }()),
+        multi_wallet_mock([&] {
+          MultiWalletMock mock;
+          mock.wallets.emplace_back(&wallet);
+          return mock;
+        }()) {}
+
+  fakeit::Mock<staking::Network> network_mock;
+  fakeit::Mock<staking::ActiveChain> chain_mock;
+  fakeit::Mock<proposer::Logic> logic_mock;
+
+  std::unique_ptr<Proposer> GetProposer() {
+    return Proposer::New(
+        &settings,
+        behavior.get(),
+        &multi_wallet_mock,
+        &network_mock.get(),
+        &chain_mock.get(),
+        &logic_mock.get());
+  }
+};
+
+}  // namespace
+
 BOOST_AUTO_TEST_SUITE(proposer_tests)
 
-fakeit::Mock<staking::Network> networkMock;
-fakeit::Mock<staking::ActiveChain> chainMock;
-
-Dependency<staking::Network> network = &networkMock.get();
-Dependency<staking::ActiveChain> chain = &chainMock.get();
+BOOST_FIXTURE_TEST_CASE(start_stop, Fixture) {
+  BOOST_CHECK_NO_THROW([&] {
+    auto p = GetProposer();
+    p->Start();
+  });
+}
 
 BOOST_AUTO_TEST_SUITE_END()
