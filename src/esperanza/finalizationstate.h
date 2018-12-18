d023532876f1251004b9652f6aa50ee6d2f3a8ad
Stanislav Frolov <stanislav@thirdhash.com>
2018-12-18 11:06:24 +0300

    undo block->pint renaming
    Signed-off-by: Stanislav Frolov <stanislav@thirdhash.com>

--- src/esperanza/finalizationstate.h
+++ src/esperanza/finalizationstate.h
@@ -225,7 +225,7 @@ class FinalizationState : public FinalizationStateData {
                     const esperanza::AdminParams &adminParams);
 
   //! \brief Returns the finalization state for the given block.
-  static FinalizationState *GetState(const CBlockIndex *pindex = nullptr);
+  static FinalizationState *GetState(const CBlockIndex *block = nullptr);
 
   static uint32_t GetEpoch(const CBlockIndex &blockIndex);
   static uint32_t GetEpoch(int blockHeight);
