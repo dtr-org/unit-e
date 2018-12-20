1aa0ce7702b6f7a15f604433b6f94d36e2af0e16
Julian Fleischer <scravy@users.noreply.github.com>
2018-12-20 10:35:15 +0100

    Remove fakeit mocking test dependency (#396)
    Signed-off-by: Julian Fleischer <julian@thirdhash.com>

--- src/test/proposer/proposer_logic_tests.cpp
+++ src/test/proposer/proposer_logic_tests.cpp
@@ -10,20 +10,6 @@
 
 #include <thread>
 
-#if defined(__GNUG__) and not defined(__clang__)
-// Fakeit does not work with GCC's devirtualization
-// which is enabled with -O2 (the default) or higher.
-#pragma GCC optimize("no-devirtualize")
-#endif
-
-#include <test/fakeit/fakeit.hpp>
-
 BOOST_AUTO_TEST_SUITE(proposer_tests)
-
-fakeit::Mock<staking::Network> networkMock;
-fakeit::Mock<staking::ActiveChain> chainMock;
-
-Dependency<staking::Network> network = &networkMock.get();
-Dependency<staking::ActiveChain> chain = &chainMock.get();
 
 BOOST_AUTO_TEST_SUITE_END()
