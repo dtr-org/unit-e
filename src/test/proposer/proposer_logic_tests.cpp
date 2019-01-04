ee67d6b505a37513efaacc48f433fc7dd03929b1
Julian Fleischer <julian@thirdhash.com>
2019-01-04 16:42:39 +0100

    Add some meaningful assertions
    Signed-off-by: Julian Fleischer <julian@thirdhash.com>

--- src/test/proposer/proposer_logic_tests.cpp
+++ src/test/proposer/proposer_logic_tests.cpp
@@ -90,12 +90,35 @@ BOOST_AUTO_TEST_CASE(propose) {
   const uint256 t1 = uint256S("01c22ebd3beef7e855b99aa6fb203e1055656c39f3c139cd4e4b13be22ee009a");
   const uint256 t2 = uint256S("8551913c6386e7771f33e31d2e77c4941e70174a96079cd591ff823fee986e48");
   const uint256 t3 = uint256S("c6c9b8db7a83122b6680ce762d6215f1eab2bd5825cfeef903de70cbf41a9803");
+  const uint256 k1 = uint256S("622cb3371c1a08096eaac564fb59acccda1fcdbe13a9dd10b486e6463c8c2525");
+  const uint256 k2 = uint256S("de2157f24915d2fb7e8bb62cfc8adc81029a7b7909e503b79aac0900195d1f5c");
+  const uint256 k3 = uint256S("6738ef1b0509836ea7a0fcc2f31887930454c96bb9c7bf2f6b04adbe2bb0d290");
   const std::vector<staking::Coin> coins{
       staking::Coin{t1, 7, 20, 1},
       staking::Coin{t2, 2, 50, 1},
       staking::Coin{t3, 4, 70, 1}};
-  LOCK(f.active_chain_mock.GetLock());
-  const auto coin = logic->TryPropose(coins);
+  f.stake_validator_mock.checkkernelfunc = [&](uint256 kernel) {
+    return kernel == k2;
+  };
+  f.stake_validator_mock.computekernelfunc = [&](const CBlockIndex *, const staking::Coin &coin, blockchain::Time) {
+    if (coin.txid == t1) {
+      return k1;
+    }
+    if (coin.txid == t2) {
+      return k2;
+    }
+    if (coin.txid == t3) {
+      return k3;
+    }
+  };
+  const auto coin = [&] {
+    LOCK(f.active_chain_mock.GetLock());
+    return logic->TryPropose(coins);
+  }();
+  BOOST_REQUIRE(static_cast<bool>(coin));
+  const proposer::EligibleCoin eligible_coin = *coin;
+  BOOST_CHECK_EQUAL(eligible_coin.kernel_hash, k2);
+  BOOST_CHECK_EQUAL(eligible_coin.utxo.txid, coins[1].txid);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
