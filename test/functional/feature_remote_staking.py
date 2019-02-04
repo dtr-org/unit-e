0bb137d49762fee2fa0ea506cd1226ff5c6b94d2
Mihai Ciumeica <mihai@thirdhash.com>
2019-02-04 16:05:11 +0100

    Add test for stakeable balance
    Signed-off-by: Mihai Ciumeica <mihai@thirdhash.com>

--- test/functional/feature_remote_staking.py
+++ test/functional/feature_remote_staking.py
@@ -5,29 +5,40 @@
 from test_framework.test_framework import UnitETestFramework
 
 
+GENESIS_KEY = 'swap fog boost power mountain pair gallery crush price fiscal thing supreme chimney drastic grab acquire any cube cereal another jump what drastic ready'
+
+
 class RemoteStakingTest(UnitETestFramework):
     def set_test_params(self):
         self.num_nodes = 2
         self.setup_clean_chain = True
+        self.extra_args=[
+            ['-proposing=0'],
+            ['-proposing=1', '-minimumchainwork=0', '-maxtipage=1000000000']
+        ]
 
     def run_test(self):
         alice, bob = self.nodes
+        alice.importmasterkey(GENESIS_KEY)
+
         dummy_addr = alice.getnewaddress()
         bobs_addr = bob.getnewaddress()
-
-        alice.generatetoaddress(101, dummy_addr)
-        self.sync_all()
 
         # Estimate staking fee
         recipient = {"address": bobs_addr, "amount": 1}
         result = alice.stakeat(recipient, True)
         assert(result['fee'] < 0.001)
 
+        bob.proposerwake()
+
         # Stake the funds
-        recipient = {"address": bobs_addr, "amount": 1}
         result = alice.stakeat(recipient)
-        alice.generate(1)
         self.sync_all()
+
+        # Bob should be able to stake the newly received coin
+        ps = bob.proposerstatus()
+        assert(ps['wallets'][0]['stakeable_balance'] > 0)
+        assert(ps['status'] == 'IS_PROPOSING')
 
         # Coinbase, change output, and the balance staked remotely
         assert(len(alice.listunspent()) == 3)
