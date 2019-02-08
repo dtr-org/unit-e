2f696fdcd453c7a7548389acdf7d61893da5e62c
Mihai Ciumeica <mihai@thirdhash.com>
2019-02-08 10:45:27 +0100

    Do not hardcode genesis block mnemonics
    Signed-off-by: Mihai Ciumeica <mihai@thirdhash.com>

--- test/functional/feature_remote_staking.py
+++ test/functional/feature_remote_staking.py
@@ -2,11 +2,9 @@
 # Copyright (c) 2019 The Unit-e Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
+from test_framework.regtest_mnemonics import regtest_mnemonics
 from test_framework.test_framework import UnitETestFramework
 from test_framework.util import assert_equal, assert_greater_than
-
-
-GENESIS_KEY = 'swap fog boost power mountain pair gallery crush price fiscal thing supreme chimney drastic grab acquire any cube cereal another jump what drastic ready'
 
 
 class RemoteStakingTest(UnitETestFramework):
@@ -20,7 +18,7 @@ class RemoteStakingTest(UnitETestFramework):
 
     def run_test(self):
         alice, bob = self.nodes
-        alice.importmasterkey(GENESIS_KEY)
+        alice.importmasterkey(regtest_mnemonics[0]['mnemonics'])
 
         alices_addr = alice.getnewaddress()
 
