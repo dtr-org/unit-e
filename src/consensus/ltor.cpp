6b209b530cbfde5e11f71a2640f3cbf24b5828d7
Andres Correa Casablanca <andres@thirdhash.com>
2019-02-12 14:03:52 +0100

    Add guard clause to SortTransactionsWithLTOR
    Signed-off-by: Andres Correa Casablanca <andres@thirdhash.com>

--- src/consensus/ltor.cpp
+++ src/consensus/ltor.cpp
@@ -8,11 +8,12 @@
 namespace ltor {
 
 void SortTransactionsWithLTOR(std::vector<CTransactionRef> &transactions) {
+    if (transactions.size() <= 1) {
+        return;
+    }
+
     // LTOR/CTOR: We ensure that all transactions (except the 0th, coinbase) are
-    // sorted in lexicographical order. Notice that we don't sort
-    // blocktemplate->vTxFees nor blocktemplate->vTxSigOpsCost because they are
-    // not used at all. These two vectors are just a "residue" from Bitcoin's
-    // PoW mining pools code.
+    // sorted in lexicographical order.
     std::sort(
         std::begin(transactions) + 1, std::end(transactions),
         [](const CTransactionRef &a, const CTransactionRef &b) -> bool {
