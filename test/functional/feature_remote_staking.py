74928f7b93cafaa66da311855ec17ff5022b227f
Azat Nizametdinov <azat@thirdhash.com>
2019-02-14 12:31:14 +0100

    Fix feature_remote_staking for Python 3.4
    Signed-off-by: Azat Nizametdinov <azat@thirdhash.com>

--- test/functional/feature_remote_staking.py
+++ test/functional/feature_remote_staking.py
@@ -6,7 +6,7 @@ from test_framework.mininode import sha256
 from test_framework.regtest_mnemonics import regtest_mnemonics
 from test_framework.script import CScript, OP_2, hash160
 from test_framework.test_framework import UnitETestFramework
-from test_framework.util import assert_equal, assert_greater_than, hex_str_to_bytes, wait_until
+from test_framework.util import assert_equal, assert_greater_than, bytes_to_hex_str, hex_str_to_bytes, wait_until
 
 
 def stake_p2wsh(node, staking_node, amount):
@@ -26,7 +26,7 @@ def stake_p2wsh(node, staking_node, amount):
     staking_key_hash = hash160(hex_str_to_bytes(addr_info['pubkey']))
 
     rs_p2wsh = CScript([OP_2, staking_key_hash, spending_script_hash])
-    outputs = [{'address': 'script', 'amount': amount, 'script': rs_p2wsh.hex()}]
+    outputs = [{'address': 'script', 'amount': amount, 'script': bytes_to_hex_str(rs_p2wsh)}]
     node.sendtypeto('unite', 'unite', outputs)
 
 
