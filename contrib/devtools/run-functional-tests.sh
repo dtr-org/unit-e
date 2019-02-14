9505ec49f27fc6fc640b40809198ec7d926cd072
Julian Fleischer <julian@thirdhash.com>
2019-02-14 17:00:28 +0100

    Amend number of CPU cores to two thirds such that also weaker machines run the tests successfully
    Signed-off-by: Julian Fleischer <julian@thirdhash.com>

--- contrib/devtools/run-functional-tests.sh
+++ contrib/devtools/run-functional-tests.sh
@@ -3,7 +3,7 @@
 # usage:
 # - run contrib/devtools/run-functional-tests.sh from the repository root
 
-NUM_CORES=$(python -c 'import multiprocessing; print (multiprocessing.cpu_count())')
+NUM_CORES=$(python -c 'import multiprocessing; print (max(4, multiprocessing.cpu_count() // 3 * 2))')
 
 test/functional/test_runner.py -j ${NUM_CORES}
 
