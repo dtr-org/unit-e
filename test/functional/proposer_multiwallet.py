9d008b0161d2246584a1dcfebd3a97e7fe70e8bc
Julian Fleischer <julian@thirdhash.com>
2019-02-18 13:42:50 +0100

    Fix flaky proposer-multiwallet functional test
    Signed-off-by: Julian Fleischer <julian@thirdhash.com>

--- test/functional/proposer_multiwallet.py
+++ test/functional/proposer_multiwallet.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2018 The Unit-e developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
-from test_framework.util import *
+from test_framework.util import assert_equal, wait_until
 from test_framework.test_framework import UnitETestFramework
 
 
@@ -19,13 +19,21 @@ class ProposerMultiwalletTest(UnitETestFramework):
 
         status = self.nodes[0].proposerstatus()['wallets']
         assert_equal(len(status), self.num_wallets)
+
+        self.log.info("Checking that every wallet has a proposer")
         for i in range(0, self.num_wallets):
             assert_equal(status[i]['wallet'], 'w{0}.dat'.format(i))
-            # the following checks that the proposer has advanced each
-            # wallet from NOT_PROPOSING to some other state (which may
-            # still be NOT_PROPOSING_SYNCHONG_BLOCKCHAIN or some other
-            # message).
-            assert(not status[i]['status'] == 'NOT_PROPOSING')
+
+        # the following checks that the proposer has advanced each
+        # wallet from NOT_PROPOSING to some other state (which may
+        # still be NOT_PROPOSING_SYNCHONG_BLOCKCHAIN or some other
+        # message).
+        def all_have_switched_away_from_not_proposing_state():
+            status = self.nodes[0].proposerstatus()['wallets']
+            return all([not status[i]['status'] == 'NOT_PROPOSING' for i in range(0, self.num_wallets)])
+
+        self.log.info("Waiting for all proposers to have advanced to some state")
+        wait_until(all_have_switched_away_from_not_proposing_state, timeout=10)
 
         print("Test succeeded.")
 
