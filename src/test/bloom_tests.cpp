0161f79c82c8c84762ff36a358bb8d1b669e6076
Mihai Ciumeica <mihai@thirdhash.com>
2019-03-01 14:50:41 +0100

    Change base58 and bech32 literals to account for removing mainnet
    Signed-off-by: Mihai Ciumeica <mihai@thirdhash.com>

--- src/test/bloom_tests.cpp
+++ src/test/bloom_tests.cpp
@@ -84,7 +84,7 @@ BOOST_AUTO_TEST_CASE(bloom_create_insert_serialize_with_tweak)
 
 BOOST_AUTO_TEST_CASE(bloom_create_insert_key)
 {
-    std::string strSecret = std::string("5Kg1gnAjaLfKiwhhPpGS3QfRg2m6awQvaj98JCZBZQ5SuS2F15C");
+    std::string strSecret = std::string("93SeGWzHAZjTh1Cz2AALv1DPKh7ok6x7vg15Npugu8pVgVAzKyk");
     CUnitESecret vchSecret;
     BOOST_CHECK(vchSecret.SetString(strSecret));
 
