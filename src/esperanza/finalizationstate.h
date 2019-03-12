471576dde626e3cda4a0dd6e7ce745474ea9158f
Stanislav Frolov <stanislav@thirdhash.com>
2019-03-12 14:28:44 +0100

    Move FinalizationStateData to the separate file
    Signed-off-by: Stanislav Frolov <stanislav@thirdhash.com>

--- src/esperanza/finalizationstate.h
+++ src/esperanza/finalizationstate.h
@@ -6,17 +6,9 @@
 #define UNITE_ESPERANZA_FINALIZATIONSTATE_H
 
 #include <better-enums/enum.h>
-#include <chain.h>
 #include <esperanza/admincommand.h>
-#include <esperanza/adminstate.h>
-#include <esperanza/checkpoint.h>
 #include <esperanza/finalizationparams.h>
-#include <esperanza/validator.h>
-#include <primitives/block.h>
-#include <sync.h>
-#include <ufp64.h>
-#include <uint256.h>
-#include <utility>
+#include <esperanza/finalizationstate_data.h>
 
 class CChainParams;
 
@@ -54,90 +46,6 @@ BETTER_ENUM(
   ADMIN_NOT_AUTHORIZED
 )
 // clang-format on
-
-/**
- * This class is the base data-class with all the data required by
- * FinalizationState. If you need to add new data member to FinalizationState
- * you probably would add it here.
- */
-class FinalizationStateData {
- public:
-  bool operator==(const FinalizationStateData &other) const;
-
- protected:
-  FinalizationStateData(const AdminParams &adminParams);
-  FinalizationStateData(const FinalizationStateData &) = default;
-  FinalizationStateData(FinalizationStateData &&) = default;
-  ~FinalizationStateData() = default;
-
-  /**
-   * A quick comment on the types chosen to represent the various class members:
-   * uint32_t - is enough to represent any epoch (even with one epoch a second
-   * it would last ~136 yrs)
-   * uint64_t - is enough to represent any amount of UNIT-E coins
-   * (total_supply=(e * 10^17) and log2(total_supply)=~58 )
-   * ufp64_t - is a way to represent a decimal number with integer part up to
-   * 10E9 and decimal part with precision of 10E-8. Using this type is safe as
-   * long as the above conditions are met. For example multiplications between
-   * ufp64t and uint64_t are for example safe since for the intermediate step a
-   * bigger int type is used, but if the result is not representable by 32 bits
-   * then the final value will overflow.
-   */
-
-  // Map of epoch number to checkpoint
-  std::map<uint32_t, Checkpoint> m_checkpoints;
-
-  // Map of epoch number to dynasty number
-  std::map<uint32_t, uint32_t> m_epochToDynasty;
-
-  // Map of dynasty number to the starting epoch number
-  std::map<uint32_t, uint32_t> m_dynastyStartEpoch;
-
-  // List of validators
-  std::map<uint160, Validator> m_validators;
-
-  // Map of the dynasty number with the delta in deposits with the previous one
-  std::map<uint32_t, uint64_t> m_dynastyDeltas;
-
-  // Map of the epoch number with the deposit scale factor
-  std::map<uint32_t, ufp64::ufp64_t> m_depositScaleFactor;
-
-  // Map of the epoch number with the running total of deposits slashed
-  std::map<uint32_t, uint64_t> m_totalSlashed;
-
-  // The current epoch number
-  uint32_t m_currentEpoch = 0;
-
-  // The current dynasy number
-  uint32_t m_currentDynasty = 0;
-
-  // Total scaled deposits in the current dynasty
-  uint64_t m_curDynDeposits = 0;
-
-  // Total scaled deposits in the previous dynasty
-  uint64_t m_prevDynDeposits = 0;
-
-  // Expected epoch of the vote source
-  uint32_t m_expectedSourceEpoch = 0;
-
-  // Number of the last finalized epoch
-  uint32_t m_lastFinalizedEpoch = 0;
-
-  // Number of the last justified epoch
-  uint32_t m_lastJustifiedEpoch = 0;
-
-  // Last checkpoint
-  const CBlockIndex *m_recommendedTarget = nullptr;
-
-  ufp64::ufp64_t m_lastVoterRescale = 0;
-
-  ufp64::ufp64_t m_lastNonVoterRescale = 0;
-
-  // Reward for voting as fraction of deposit size
-  ufp64::ufp64_t m_rewardFactor = 0;
-
-  AdminState m_adminState;
-};
 
 /**
  * This class is thread safe, any public method that is actually changing
---
+++ src/esperanza/finalizationstate_data.cpp
@@ -0,0 +1,30 @@
+#include <esperanza/finalizationstate_data.h>
+
+namespace esperanza {
+
+FinalizationStateData::FinalizationStateData(const AdminParams &adminParams)
+    : m_adminState(adminParams) {}
+
+bool FinalizationStateData::operator==(const FinalizationStateData &other) const {
+  return m_checkpoints == other.m_checkpoints &&
+         m_epochToDynasty == other.m_epochToDynasty &&
+         m_dynastyStartEpoch == other.m_dynastyStartEpoch &&
+         m_validators == other.m_validators &&
+         m_dynastyDeltas == other.m_dynastyDeltas &&
+         m_depositScaleFactor == other.m_depositScaleFactor &&
+         m_totalSlashed == other.m_totalSlashed &&
+         m_currentEpoch == other.m_currentEpoch &&
+         m_currentDynasty == other.m_currentDynasty &&
+         m_curDynDeposits == other.m_curDynDeposits &&
+         m_prevDynDeposits == other.m_prevDynDeposits &&
+         m_expectedSourceEpoch == other.m_expectedSourceEpoch &&
+         m_lastFinalizedEpoch == other.m_lastFinalizedEpoch &&
+         m_lastJustifiedEpoch == other.m_lastJustifiedEpoch &&
+         m_recommendedTarget == other.m_recommendedTarget &&
+         m_lastVoterRescale == other.m_lastVoterRescale &&
+         m_lastNonVoterRescale == other.m_lastNonVoterRescale &&
+         m_rewardFactor == other.m_rewardFactor &&
+         m_adminState == other.m_adminState;
+}
+
+}  // namespace esperanza
