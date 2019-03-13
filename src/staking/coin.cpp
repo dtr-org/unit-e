96b2c9efc96c9618a7d62894cdb148fa73923d17
Julian Fleischer <julian@thirdhash.com>
2019-03-13 10:20:37 +0100

    Inline things
    Signed-off-by: Julian Fleischer <julian@thirdhash.com>

--- src/staking/coin.cpp
+++ src/staking/coin.cpp
@@ -11,66 +11,9 @@
 
 namespace staking {
 
-Coin::Coin(const CBlockIndex *containing_block, const COutPoint &out_point, const CTxOut &tx_out)
-    : containing_block(containing_block), out_point(out_point), tx_out(tx_out) {}
-
-const uint256 &Coin::GetBlockHash() const {
-  return *containing_block->phashBlock;
-}
-
-blockchain::Time Coin::GetBlockTime() const {
-  return containing_block->nTime;
-}
-
-blockchain::Height Coin::GetHeight() const {
-  return static_cast<blockchain::Height>(containing_block->nHeight);
-}
-
-std::uint32_t Coin::GetOutputIndex() const {
-  return out_point.n;
-}
-
-CAmount Coin::GetAmount() const {
-  return tx_out.nValue;
-}
-
-const CScript &Coin::GetScriptPubKey() const {
-  return tx_out.scriptPubKey;
-}
-
-const uint256 &Coin::GetTransactionHash() const {
-  return out_point.hash;
-}
-
-const COutPoint &Coin::GetOutPoint() const {
-  return out_point;
-}
-
 std::string Coin::ToString() const {
   return tfm::format("Coin(txid=%s,index=%d,amount=%d,height=%d)",
                      util::to_string(GetTransactionHash()), GetOutputIndex(), GetAmount(), GetHeight());
-}
-
-bool CoinByAmountComparator::operator()(const Coin &left, const Coin &right) const {
-  if (left.GetAmount() > right.GetAmount()) {
-    return true;
-  }
-  if (left.GetAmount() < right.GetAmount()) {
-    return false;
-  }
-  if (left.GetHeight() < right.GetHeight()) {
-    return true;
-  }
-  if (left.GetHeight() > right.GetHeight()) {
-    return false;
-  }
-  if (left.GetTransactionHash() < right.GetTransactionHash()) {
-    return true;
-  }
-  if (left.GetTransactionHash() != right.GetTransactionHash()) {
-    return false;
-  }
-  return left.GetOutputIndex() < right.GetOutputIndex();
 }
 
 }  // namespace staking
--- src/staking/coin.h
+++ src/staking/coin.h
@@ -21,35 +21,36 @@ namespace staking {
 class Coin {
 
  public:
-  Coin(const CBlockIndex *containing_block, const COutPoint &out_point, const CTxOut &tx_out);
+  Coin::Coin(const CBlockIndex *containing_block, const COutPoint &out_point, const CTxOut &tx_out)
+      : containing_block(containing_block), out_point(out_point), tx_out(tx_out) {}
 
   //! \brief The hash of the block containing the staked coin.
-  const uint256 &GetBlockHash() const;
+  const uint256 &GetBlockHash() const { return *containing_block->phashBlock; }
 
   //! \brief The time of the block containing the staked coin.
-  blockchain::Time GetBlockTime() const;
+  blockchain::Time GetBlockTime() const { return containing_block->nTime; };
 
   //! \brief The amount of stake.
-  CAmount GetAmount() const;
+  CAmount GetAmount() const { return tx_out.nValue; };
 
   //! \brief The height at which this coin is included in a block.
-  blockchain::Height GetHeight() const;
+  blockchain::Height GetHeight() const { return static_cast<blockchain::Height>(containing_block->nHeight); }
 
   //! \brief The id of the transaction which spends this piece of stake.
   //!
   //! This is the same as `GetOutPoint.hash`.
-  const uint256 &GetTransactionHash() const;
+  const uint256 &GetTransactionHash() const { return out_point.hash; }
 
   //! \brief The index of the spending output.
   //!
   //! This is the same as `GetOutPoint.n`
-  std::uint32_t GetOutputIndex() const;
+  std::uint32_t GetOutputIndex() const { return out_point.n; }
 
   //! \brief The outpoint of the staking output (txid and out index).
-  const COutPoint &GetOutPoint() const;
+  const COutPoint &GetOutPoint() const { return out_point; }
 
   //! \brief The locking script of the coin.
-  const CScript &GetScriptPubKey() const;
+  const CScript &GetScriptPubKey() const { return tx_out.scriptPubKey; }
 
   bool operator==(const Coin &other) const {
     return GetOutPoint() == other.GetOutPoint();
@@ -72,8 +73,45 @@ class Coin {
   CTxOut tx_out;
 };
 
+//! \brief A comparator that compares coins by amount.
+//!
+//! Compares coins by their properties in the following order:
+//! - Amount, descending (bigger coins first)
+//! - Height, ascending (older coins first)
+//! - TransactionHash, ascending
+//! - OutputIndex, ascending
+//!
+//! This is not an intrinsic compare function on Coin as this
+//! is in no way how coins would be sorted in the general case.
+//! While the properties Amount and Height should always be the
+//! same for two coins for which the OutPoint is the same, a user
+//! of this class might not follow this rule (for example in tests)
+//! in which case `==` and `!=` might differ from `<`. In other words:
+//! A proper `<` on Coin would take into account only the properties
+//! which `==` and `!=` take into account, but this comparator takes
+//! into account more properties.
 struct CoinByAmountComparator {
-  bool operator()(const Coin &left, const Coin &right) const;
+  bool operator()(const Coin &left, const Coin &right) const {
+    if (left.GetAmount() > right.GetAmount()) {
+      return true;
+    }
+    if (left.GetAmount() < right.GetAmount()) {
+      return false;
+    }
+    if (left.GetHeight() < right.GetHeight()) {
+      return true;
+    }
+    if (left.GetHeight() > right.GetHeight()) {
+      return false;
+    }
+    if (left.GetTransactionHash() < right.GetTransactionHash()) {
+      return true;
+    }
+    if (left.GetTransactionHash() != right.GetTransactionHash()) {
+      return false;
+    }
+    return left.GetOutputIndex() < right.GetOutputIndex();
+  }
 };
 
 using CoinSet = std::set<Coin, CoinByAmountComparator>;
