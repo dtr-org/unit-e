9452104949757f73033212d9b9e911ae3dc284f6
Mihai Ciumeica <mihai@thirdhash.com>
2019-03-14 10:34:32 +0100

    Fixed wallet_hwsign.py
    Signed-off-by: Mihai Ciumeica <mihai@thirdhash.com>
    Signed-off-by: Mateusz Morusiewicz <mateusz@thirdhash.com>

--- test/functional/wallet_hwsign.py
+++ test/functional/wallet_hwsign.py
@@ -2,32 +2,31 @@
 # Copyright (c) 2019 The Unit-e developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
-from test_framework.test_framework import UnitETestFramework
-from test_framework.util import assert_equal
+from test_framework.test_framework import UnitETestFramework, COINBASE_MATURITY
+from test_framework.util import assert_equal, sync_mempools
 
 
 class UsbDeviceCryptoTest(UnitETestFramework):
     def set_test_params(self):
         self.num_nodes = 2
         self.setup_clean_chain = True
+        self.extra_args = [['-stakesplitthreshold=1000000000']] * 2
 
     def run_test(self):
         hw_node, other_node = self.nodes
+        self.setup_stake_coins(other_node)
 
         hw_node.initaccountfromdevice()
 
         assert_equal(len(hw_node.listunspent()), 0)
-        assert_equal(len(other_node.listunspent()), 0)
-
-        money_addr = other_node.getnewaddress()
-        other_node.generatetoaddress(102, money_addr)
-        self.sync_all()
+        assert_equal(len(other_node.listunspent()), 1)
 
         balance = hw_node.getbalance()
         assert_equal(balance, 0)
 
+        other_node.generate(1)
         balance = other_node.getbalance()
-        assert_equal(balance, 100)
+        assert_equal(balance, other_node.initial_stake)
 
         # Transaction with one input
         addr = hw_node.getnewaddress()
@@ -38,8 +37,10 @@ class UsbDeviceCryptoTest(UnitETestFramework):
         balance = hw_node.getbalance()
         assert_equal(balance, 1)
 
+        # Send the money back
         hw_node.sendtoaddress(other_node.getnewaddress(), 1, None, None, True)
-        hw_node.generate(1)
+        sync_mempools(self.nodes)
+        other_node.generate(1)
         self.sync_all()
 
         # Transaction with two inputs
@@ -51,10 +52,14 @@ class UsbDeviceCryptoTest(UnitETestFramework):
         balance = hw_node.getbalance()
         assert_equal(balance, 0.5)
 
+        # Send the money back
         hw_node.sendtoaddress(other_node.getnewaddress(), 0.5, None, None, True)
-        hw_node.generate(1)
+        sync_mempools(self.nodes)
+        other_node.generate(1)
         self.sync_all()
-        assert(other_node.getbalance() > 99.9)
+
+        # Check final balance
+        assert(other_node.getbalance() > other_node.initial_stake - 0.01)
 
 
 if __name__ == '__main__':
