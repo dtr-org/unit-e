abb76fdf86fc73e19304d424783166d453302418
Matteo Sumberaz <matteo@thirdhash.com>
2019-03-15 10:52:33 +0100

    Remove unnecessary -par
    Signed-off-by: Matteo Sumberaz <matteo@thirdhash.com>
    Signed-off-by: Mateusz Morusiewicz <mateusz@thirdhash.com>

--- test/functional/esperanza_expired_vote_conflict.py
+++ test/functional/esperanza_expired_vote_conflict.py
@@ -29,7 +29,7 @@ class ExpiredVoteConflict(UnitETestFramework):
         })
 
         self.extra_args = [
-            ['-debug=all', '-whitelist=127.0.0.1','-par=-10',
+            ['-debug=all', '-whitelist=127.0.0.1',
              '-esperanzaconfig=' + finalization_params],
 
             ['-validating=1', '-debug=all',
