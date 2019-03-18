2a4c072bb51e940aaa830dc101cb36e0b2b962bc
Matteo Sumberaz <matteo@thirdhash.com>
2019-03-18 16:33:47 +0100

    Rewrte test
    Signed-off-by: Matteo Sumberaz <matteo@thirdhash.com>

--- src/test/blockchain/blockchain_parameters_tests.cpp
+++ src/test/blockchain/blockchain_parameters_tests.cpp
@@ -30,41 +30,25 @@ BOOST_AUTO_TEST_CASE(reward_function_test) {
 
   Parameters params = blockchain::Parameters::TestNet();
 
-  auto test_reward = [params](int period) -> CAmount {
-
-      CAmount base_reward = 0;
-
-      if(period < params.reward_schedule.size()) {
-        base_reward = static_cast<uint64_t>(params.reward_schedule[period]);
-      }
-      return ufp64::mul_to_uint(params.immediate_reward_fraction, base_reward);
-  };
-
   CAmount block_reward = params.reward_function(params, 0);
-  CAmount expected_reward = test_reward(0);
-  BOOST_CHECK_EQUAL(expected_reward, block_reward);
+  BOOST_CHECK_EQUAL(375000000, block_reward);
 
-  block_reward = params.reward_function(params, 1000);
-  BOOST_CHECK_EQUAL(expected_reward, block_reward);
+  BOOST_CHECK_EQUAL(375000000, block_reward);
 
   block_reward = params.reward_function(params, params.period_blocks - 1);
-  BOOST_CHECK_EQUAL(expected_reward, block_reward);
+  BOOST_CHECK_EQUAL(375000000, block_reward);
 
   block_reward = params.reward_function(params, params.period_blocks);
-  expected_reward = test_reward(1);
-  BOOST_CHECK_EQUAL(expected_reward, block_reward);
+  BOOST_CHECK_EQUAL(170000000, block_reward);
 
   block_reward = params.reward_function(params, params.period_blocks * 2);
-  expected_reward = test_reward(2);
-  BOOST_CHECK_EQUAL(expected_reward, block_reward);
+  BOOST_CHECK_EQUAL(55000000, block_reward);
 
   block_reward = params.reward_function(params, params.period_blocks * 3);
-  expected_reward = test_reward(3);
-  BOOST_CHECK_EQUAL(expected_reward, block_reward);
+  BOOST_CHECK_EQUAL(15000000, block_reward);
 
   block_reward = params.reward_function(params, params.period_blocks * 4);
-  expected_reward = test_reward(4);
-  BOOST_CHECK_EQUAL(expected_reward, block_reward);
+  BOOST_CHECK_EQUAL(3100000, block_reward);
 
   block_reward = params.reward_function(params, params.period_blocks * 5);
   BOOST_CHECK_EQUAL(0, block_reward);
