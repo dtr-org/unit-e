8ca46099bde13bcb7debb4094c7f4943e1e90b3b
Kostiantyn Stepaniuk <kostia@thirdhash.com>
2019-03-21 18:11:26 +0100

    Fix interface_zmq.py
    Signed-off-by: Kostiantyn Stepaniuk <kostia@thirdhash.com>

--- test/functional/interface_zmq.py
+++ test/functional/interface_zmq.py
@@ -108,7 +108,7 @@ class ZMQTest (UnitETestFramework):
 
             # Should receive the generated raw block.
             block = self.rawblock.receive()
-            assert_equal(genhashes[x], bytes_to_hex_str(hash256(block[:BLOCK_HEADER_LENGTH])))  # skip the header
+            assert_equal(genhashes[x], bytes_to_hex_str(hash256(block[:144])))
 
         self.log.info("Wait for tx from second node")
         payment_txid = self.nodes[1].sendtoaddress(self.nodes[0].getnewaddress(), 1.0)
