520c9bb8781d9599b188a120d5abc9d92a3116af
Matteo Sumberaz <matteo@thirdhash.com>
2019-03-27 14:03:54 +0100

    Immprove the deposit test
    Signed-off-by: Matteo Sumberaz <matteo@thirdhash.com>

--- test/functional/esperanza_deposit.py
+++ test/functional/esperanza_deposit.py
@@ -5,19 +5,18 @@
 
 from test_framework.util import (
     json,
+    assert_finalizationstate,
+    sync_blocks,
     assert_equal,
-    JSONRPCException,
-    wait_until,
     assert_raises_rpc_error,
-    assert_less_than
-)
+    assert_less_than, wait_until, connect_nodes_bi)
 from test_framework.test_framework import UnitETestFramework
 
 
 class EsperanzaDepositTest(UnitETestFramework):
 
     def set_test_params(self):
-        self.num_nodes = 4
+        self.num_nodes = 2
 
         params_data = {
             'epochLength': 10,
@@ -26,85 +25,97 @@ class EsperanzaDepositTest(UnitETestFramework):
 
         validator_node_params = [
             '-validating=1',
-            '-debug=all',
-            '-rescan=1',
-            '-whitelist=127.0.0.1',
             '-esperanzaconfig=' + json_params
         ]
-        proposer_node_params = [
-            '-debug=all',
-            '-txindex',
-            '-whitelist=127.0.0.1',
-            '-esperanzaconfig=' + json_params
-        ]
+        proposer_node_params = ['-esperanzaconfig=' + json_params]
 
-        self.extra_args = [validator_node_params,
-                           proposer_node_params,
-                           proposer_node_params,
-                           proposer_node_params]
+        self.extra_args = [proposer_node_params,
+                           validator_node_params]
         self.setup_clean_chain = True
 
-    def run_test(self):
-        nodes = self.nodes
-        validator = nodes[0]
+    # create topology where arrows denote non-persistent connection
+    # finalizer ←→ proposer
+    def setup_network(self):
+        self.setup_nodes()
 
-        self.setup_stake_coins(*self.nodes)
-        assert_equal(validator.getbalance(), validator.initial_stake)
+        proposer = self.nodes[0]
+        finalizer = self.nodes[1]
+
+        connect_nodes_bi(self.nodes, finalizer.index, proposer.index)
+
+    def run_test(self):
+        proposer = self.nodes[0]
+        finalizer = self.nodes[1]
+
+        self.setup_stake_coins(proposer, finalizer)
+        assert_equal(finalizer.getbalance(), finalizer.initial_stake)
 
         # Leave IBD
-        self.generate_sync(nodes[1])
+        proposer.generate(1)
+        sync_blocks([proposer, finalizer])
 
-        test_not_enough_money_for_deposit(validator)
-        test_deposit_too_small(validator)
-        self.test_successful_deposit(validator)
-        test_duplicate_deposit(validator)
+        test_not_enough_money_for_deposit(finalizer)
+        test_deposit_too_small(finalizer)
+        self.test_successful_deposit(finalizer, proposer)
+        test_duplicate_deposit(finalizer)
 
-    def test_successful_deposit(self, validator):
-        nodes = self.nodes
+    def test_successful_deposit(self, finalizer, proposer):
 
-        payto = validator.getnewaddress("", "legacy")
-        txid = validator.deposit(payto, 1500)
+        payto = finalizer.getnewaddress("", "legacy")
+        txid = finalizer.deposit(payto, 1500)
 
-        deposit_tx = validator.gettransaction(txid)
-        assert_equal(deposit_tx['amount'], 0)  # we send the money to ourselves
+        deposit_tx = finalizer.gettransaction(txid)
+        assert_equal(deposit_tx['amount'], 0)  # 0 because we send the money to ourselves
         assert_less_than(deposit_tx['fee'], 0)  # fee returned by gettransaction is negative
 
-        raw_deposit_tx = validator.decoderawtransaction(deposit_tx['hex'])
+        raw_deposit_tx = finalizer.decoderawtransaction(deposit_tx['hex'])
         assert_equal(raw_deposit_tx['vout'][0]['value'], 1500)
         assert_equal(raw_deposit_tx['vout'][1]['value'], 10000 - 1500 + deposit_tx['fee'])
 
         # wait for transaction to propagate
-        self.wait_for_transaction(txid, 60)
+        self.wait_for_transaction(txid, 10)
 
-        wait_until(lambda: validator.getvalidatorinfo()['validator_status'] == 'WAITING_DEPOSIT_CONFIRMATION',
+        wait_until(lambda: finalizer.getvalidatorinfo()['validator_status'] == 'WAITING_DEPOSIT_CONFIRMATION',
                    timeout=5)
 
         # mine a block to allow the deposit to get included
-        self.generate_sync(nodes[2])
+        self.generate_sync(proposer)
 
-        wait_until(lambda: validator.getvalidatorinfo()['validator_status'] == 'WAITING_DEPOSIT_FINALIZATION',
+        wait_until(lambda: finalizer.getvalidatorinfo()['validator_status'] == 'WAITING_DEPOSIT_FINALIZATION',
                    timeout=5)
 
-        # the validator will be ready to operate in epoch 4
-        # TODO: UNIT - E: it can be 2 epochs as soon as #572 is fixed
-        for n in range(0, 39):
-            self.generate_block(nodes[(n % 3) + 1])
+        assert_equal(proposer.getblockcount(), 2)
+        assert_finalizationstate(proposer, {'currentEpoch': 0,
+                                            'currentDynasty': 0,
+                                            'lastJustifiedEpoch': 0,
+                                            'lastFinalizedEpoch': 0,
+                                            'validators': 0})
 
-        wait_until(lambda: validator.getvalidatorinfo()['enabled'] == 1, timeout=5)
-        assert_equal(validator.getvalidatorinfo()['validator_status'], 'IS_VALIDATING')
+        # the finalizer will be ready to operate at the beginning of epoch 4
+        # TODO: UNIT-E: it can be 3 epochs as soon as #572 is fixed
+        proposer.generate(38)
+        assert_equal(proposer.getblockcount(), 40)
+        sync_blocks([proposer, finalizer])
 
-    def generate_block(self, node):
-        i = 0
-        # It is rare but possible that a block was valid at the moment of creation but
-        # invalid at submission. This is to account for those cases.
-        while i < 5:
-            try:
-                self.generate_sync(node)
-                return
-            except JSONRPCException as exp:
-                i += 1
-                print("error generating block:", exp.error)
-        raise AssertionError("Node" + str(node.index) + " cannot generate block")
+        assert_finalizationstate(proposer, {'currentEpoch': 4,
+                                            'currentDynasty': 2,
+                                            'lastJustifiedEpoch': 3,
+                                            'lastFinalizedEpoch': 2,
+                                            'validators': 0})
+
+        wait_until(lambda: finalizer.getvalidatorinfo()['enabled'] == 1, timeout=5)
+        assert_equal(finalizer.getvalidatorinfo()['validator_status'], 'IS_VALIDATING')
+
+        # The finalizer will actually join the finalizers set one dynasty later
+        proposer.generate(10)
+        assert_equal(proposer.getblockcount(), 50)
+        sync_blocks([proposer, finalizer])
+
+        assert_finalizationstate(proposer, {'currentEpoch': 5,
+                                            'currentDynasty': 3,
+                                            'lastJustifiedEpoch': 4,
+                                            'lastFinalizedEpoch': 3,
+                                            'validators': 1})
 
 
 # Deposit all you got, not enough coins left for the fees
