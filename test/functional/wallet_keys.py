4347f748596a8994b960da519f0efac0a7668ee6
Julian Fleischer <scravy@users.noreply.github.com>
2019-03-28 20:46:49 +0100

    Improve KeyTool and documentation (#859)
    Follow up to #842 
    
    Signed-off-by: Julian Fleischer <julian@thirdhash.com>

--- test/functional/wallet_keys.py
+++ test/functional/wallet_keys.py
@@ -5,10 +5,10 @@
 
 from test_framework.keytools import (
     KeyTool,
-    PublicKey,
-    PrivateKey,
 )
-from test_framework.test_framework import UnitETestFramework
+from test_framework.test_framework import (
+    UnitETestFramework,
+)
 from test_framework.util import (
     assert_equal,
 )
@@ -19,30 +19,70 @@ class WalletKeysTest(UnitETestFramework):
         self.num_nodes = 1
         self.extra_args = [[]]
 
-    def run_test(self):
-        keytool = KeyTool.for_node(self.nodes[0])
+    def test_bech32_address(self, keytool):
+        self.log.info("Checking bech32 addresses...")
 
+        self.log.info("- Get a new legacy address from the node")
         bech32_address = keytool.get_bech32_address()
+
+        self.log.info("- Lookup the private key for the newly created address from the node")
         privkey = keytool.get_privkey(bech32_address)
+
+        self.log.info("- Lookup the public key for the newly created address from the node")
         pubkey = keytool.get_pubkey(bech32_address)
 
         assert(pubkey.is_compressed())
 
-        assert_equal(pubkey.to_hex(),
-                     privkey.get_pubkey().to_hex())
+        self.log.info("- Check that addresses derived via KeyTool match the address given by the node")
         assert_equal(keytool.get_bech32_address(privkey),
                      keytool.get_bech32_address(pubkey),
                      bech32_address)
 
+    def test_legacy_address(self, keytool):
+        self.log.info("Checking legacy addresses...")
+
+        self.log.info("- Get a new legacy address from the node")
         legacy_address = keytool.get_legacy_address()
+
+        self.log.info("- Lookup the private key for the newly created address from the node")
         privkey = keytool.get_privkey(legacy_address)
+
+        self.log.info("- Lookup the public key for the newly created address from the node")
         pubkey = keytool.get_pubkey(legacy_address)
 
-        assert_equal(pubkey.to_hex(),
-                     privkey.get_pubkey().to_hex())
+        self.log.info("- Check that addresses derived via KeyTool match the address given by the node")
         assert_equal(keytool.get_legacy_address(privkey),
                      keytool.get_legacy_address(pubkey),
                      legacy_address)
+
+    def test_import_keys(self, keytool):
+        self.log.info("Checking importing keys...")
+
+        privkey = keytool.make_privkey()
+
+        self.log.info("- Import private key at node")
+        keytool.upload_key(privkey)
+
+        self.log.info("- Lookup private key by bech32 address from node")
+        bech32_address = keytool.get_bech32_address(privkey)
+        privkey_from_node = keytool.get_privkey(bech32_address)
+
+        self.log.info("- Check that generated private key and private key reported by node are equal")
+        assert_equal(privkey, privkey_from_node)
+
+        self.log.info("- Lookup private key by legacy address from node")
+        legacy_address = keytool.get_legacy_address(privkey)
+        privkey_from_node = keytool.get_privkey(legacy_address)
+
+        self.log.info("- Check that generated private key and private key reported by node are equal")
+        assert_equal(privkey, privkey_from_node)
+
+    def run_test(self):
+        keytool = KeyTool.for_node(self.nodes[0])
+
+        self.test_bech32_address(keytool)
+        self.test_legacy_address(keytool)
+        self.test_import_keys(keytool)
 
 
 if __name__ == '__main__':
