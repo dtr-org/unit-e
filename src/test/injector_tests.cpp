dtr-org/unit-e

commit 557f08c19004fccdf7ddf8f4030eaec38f77a829

Implement proper finalization state repository saving and loading (leveldb) (#793)
Introduces finalization::StateDB component which is basically an interface to load and save repository. It provides two ways to restore repository: 
* `StateDB::Load` loads every entry from the database to the repository.
* `StateDB::LoadStatesHigherThan` loads states for the blocks which height is greater than given height.

The second one is preferable as we keep only a limited set of states in memory. In other words, even when we load everything from the database, we trim the repository on the next step.

Also implements recovery for cases when finalization state database is partially broken or out-of-sync with the block-index database.

Adds `BlockIndexMap` component which is a mockable abstraction for `mapBlockIndex`.

Fixes #432.

Signed-off-by: Stanislav Frolov <stanislav@thirdhash.com>

src/Makefile.am
@@ -129,6 +129,7 @@ UNITE_CORE_H = \
   esperanza/walletextension_deps.h \
   esperanza/walletstate.h \
   extkey.h \
+  finalization/state_db.h \
   finalization/state_processor.h \
   finalization/state_repository.h \
   finalization/vote_recorder.h \
@@ -216,6 +217,7 @@ UNITE_CORE_H = \
   snapshot/snapshot_validation.h \
   snapshot/state.h \
   staking/active_chain.h \
+  staking/block_index_map.h \
   staking/block_validation_info.h \
   staking/block_validator.h \
   staking/coin.h \
@@ -299,6 +301,7 @@ libunite_server_a_SOURCES = \
   esperanza/finalizationstate.cpp \
   esperanza/finalizationstate_data.cpp \
   esperanza/validator.cpp \
+  finalization/state_db.cpp \
   finalization/state_processor.cpp \
   finalization/state_repository.cpp \
   finalization/vote_recorder.cpp \
@@ -344,6 +347,7 @@ libunite_server_a_SOURCES = \
   snapshot/snapshot_validation.cpp \
   snapshot/state.cpp \
   staking/active_chain.cpp \
+  staking/block_index_map.cpp \
   staking/block_validation_info.cpp \
   staking/block_validator.cpp \
   staking/coin.cpp \

src/finalization/state_db.cpp
@@ -1,0 +1,182 @@
+// Copyright (c) 2019 The Unit-e developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <finalization/state_db.h>
+
+#include <dbwrapper.h>
+#include <esperanza/finalizationstate.h>
+#include <staking/active_chain.h>
+#include <staking/block_index_map.h>
+#include <validation.h>
+
+namespace finalization {
+
+namespace {
+
+class StateDBImpl : public StateDB, public CDBWrapper {
+ public:
+  StateDBImpl(const StateDBParams &p,
+              Dependency<Settings> settings,
+              Dependency<staking::BlockIndexMap> block_index_map,
+              Dependency<staking::ActiveChain> active_chain)
+      : CDBWrapper(settings->data_dir / "finalization", p.cache_size, p.inmemory, p.wipe, p.obfuscate),
+        m_block_index_map(block_index_map),
+        m_active_chain(active_chain) {}
+
+  bool Save(const std::map<const CBlockIndex *, FinalizationState> &states) override;
+
+  //! \brief Loads all the states from leveldb.
+  bool Load(const esperanza::FinalizationParams &fin_params,
+            const esperanza::AdminParams &admin_params,
+            std::map<const CBlockIndex *, FinalizationState> *states) override;
+
+  //! \brief Loads specific state from leveldb.
+  bool Load(const CBlockIndex &index,
+            const esperanza::FinalizationParams &fin_params,
+            const esperanza::AdminParams &admin_params,
+            std::map<const CBlockIndex *, FinalizationState> *states) const override;
+
+  //! \brief Returns last finalized epoch accoring to active chain's tip.
+  boost::optional<uint32_t> FindLastFinalizedEpoch(
+      const esperanza::FinalizationParams &fin_params,
+      const esperanza::AdminParams &admin_params) const override;
+
+  //! \brief Load most actual states from leveldb.
+  //!
+  //! This function scans mapBlockIndex and consider to load finalization state if:
+  //! * index is on main chain and higher than `height`.
+  //! * index is a fork and its origin is higher than `height`.
+  void LoadStatesHigherThan(
+      blockchain::Height height,
+      const esperanza::FinalizationParams &fin_params,
+      const esperanza::AdminParams &admin_params,
+      std::map<const CBlockIndex *, FinalizationState> *states) const override;
+
+ private:
+  Dependency<staking::BlockIndexMap> m_block_index_map;
+  Dependency<staking::ActiveChain> m_active_chain;
+};
+
+bool StateDBImpl::Save(const std::map<const CBlockIndex *, FinalizationState> &states) {
+  CDBBatch batch(*this);
+  for (const auto &i : states) {
+    const uint256 &block_hash = i.first->GetBlockHash();
+    const FinalizationState &state = i.second;
+    batch.Write(block_hash, state);
+  }
+  return WriteBatch(batch, true);
+}
+
+bool StateDBImpl::Load(const esperanza::FinalizationParams &fin_params,
+                       const esperanza::AdminParams &admin_params,
+                       std::map<const CBlockIndex *, FinalizationState> *states) {
+
+  assert(states != nullptr);
+  AssertLockHeld(m_block_index_map->GetLock());
+
+  states->clear();
+
+  std::unique_ptr<CDBIterator> cursor(NewIterator());
+  uint256 key;
+  cursor->Seek(key);
+
+  while (cursor->Valid()) {
+    if (!cursor->GetKey(key)) {
+      return error("%s: failed to get key", __func__);
+    }
+    const CBlockIndex *block_index = m_block_index_map->Lookup(key);
+    if (block_index == nullptr) {
+      return error("%s: failed to find block index %s", __func__, util::to_string(key));
+    }
+    FinalizationState state(fin_params, admin_params);
+    if (!cursor->GetValue(state)) {
+      return error("%s: failed to get value for key %s", __func__, util::to_string(key));
+    }
+    const auto res = states->emplace(block_index, std::move(state));
+    assert(res.second);
+    cursor->Next();
+  }
+  return true;
+}
+
+bool StateDBImpl::Load(const CBlockIndex &index,
+                       const esperanza::FinalizationParams &fin_params,
+                       const esperanza::AdminParams &admin_params,
+                       std::map<const CBlockIndex *, FinalizationState> *states) const {
+
+  assert(states != nullptr);
+
+  FinalizationState state(fin_params, admin_params);
+  if (Read(index.GetBlockHash(), state)) {
+    states->emplace(&index, std::move(state));
+    return true;
+  }
+
+  return false;
+}
+
+boost::optional<uint32_t> StateDBImpl::FindLastFinalizedEpoch(
+    const esperanza::FinalizationParams &fin_params,
+    const esperanza::AdminParams &admin_params) const {
+
+  AssertLockHeld(m_active_chain->GetLock());
+
+  const CBlockIndex *walk = m_active_chain->GetTip();
+
+  while (walk != nullptr) {
+    FinalizationState state(fin_params, admin_params);
+    if (Read(walk->GetBlockHash(), state)) {
+      return state.GetLastFinalizedEpoch();
+    }
+    walk = walk->pprev;
+  }
+
+  return boost::none;
+}
+
+void StateDBImpl::LoadStatesHigherThan(
+    const blockchain::Height height,
+    const esperanza::FinalizationParams &fin_params,
+    const esperanza::AdminParams &admin_params,
+    std::map<const CBlockIndex *, FinalizationState> *states) const {
+
+  assert(states != nullptr);
+  AssertLockHeld(m_active_chain->GetLock());
+  AssertLockHeld(m_block_index_map->GetLock());
+
+  states->clear();
+
+  m_block_index_map->ForEach([states, height, &fin_params, &admin_params, this](const uint256 &block_hash, const CBlockIndex &block_index) {
+    const CBlockIndex *origin = m_active_chain->FindForkOrigin(block_index);
+    if (origin != nullptr && static_cast<blockchain::Height>(origin->nHeight) > height) {
+      FinalizationState state(fin_params, admin_params);
+      if (Read(block_hash, state)) {
+        states->emplace(&block_index, std::move(state));
+      }
+    }
+    return true;
+  });
+}
+
+}  // namespace
+
+std::unique_ptr<StateDB> StateDB::New(
+    Dependency<Settings> settings,
+    Dependency<staking::BlockIndexMap> block_index_map,
+    Dependency<staking::ActiveChain> active_chain) {
+  return NewFromParams(StateDBParams{}, settings, block_index_map, active_chain);
+}
+
+std::unique_ptr<StateDB> StateDB::NewFromParams(
+    const StateDBParams &params,
+    Dependency<Settings> settings,
+    Dependency<staking::BlockIndexMap> block_index_map,
+    Dependency<staking::ActiveChain> active_chain) {
+  if (!params.inmemory) {
+    fs::create_directories(settings->data_dir);
+  }
+  return MakeUnique<StateDBImpl>(params, settings, block_index_map, active_chain);
+}
+
+}  // namespace finalization

src/finalization/state_db.h
@@ -1,0 +1,83 @@
+// Copyright (c) 2019 The Unit-e developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef FINALIZATION_STATE_DB_H
+#define FINALIZATION_STATE_DB_H
+
+#include <blockchain/blockchain_types.h>
+#include <dependency.h>
+#include <settings.h>
+
+namespace esperanza {
+class FinalizationState;
+struct FinalizationParams;
+struct AdminParams;
+}  // namespace esperanza
+
+namespace staking {
+class ActiveChain;
+class BlockIndexMap;
+}  // namespace staking
+
+class CBlockIndex;
+
+namespace finalization {
+
+using FinalizationState = esperanza::FinalizationState;
+
+struct StateDBParams {
+  size_t cache_size = 0;
+  bool inmemory = false;
+  bool wipe = false;
+  bool obfuscate = false;
+};
+
+class StateDB {
+ public:
+  //! \brief Saves states.
+  virtual bool Save(const std::map<const CBlockIndex *, FinalizationState> &states) = 0;
+
+  //! \brief Loads all the states.
+  virtual bool Load(const esperanza::FinalizationParams &fin_params,
+                    const esperanza::AdminParams &admin_params,
+                    std::map<const CBlockIndex *, FinalizationState> *states) = 0;
+
+  //! \brief Loads specific state.
+  virtual bool Load(const CBlockIndex &index,
+                    const esperanza::FinalizationParams &fin_params,
+                    const esperanza::AdminParams &admin_params,
+                    std::map<const CBlockIndex *, FinalizationState> *states) const = 0;
+
+  //! \brief Returns last finalized epoch accoring to active chain's tip.
+  virtual boost::optional<uint32_t> FindLastFinalizedEpoch(
+      const esperanza::FinalizationParams &fin_params,
+      const esperanza::AdminParams &admin_params) const = 0;
+
+  //! \brief Load most actual states.
+  //!
+  //! This function scans mapBlockIndex and consider to load finalization state if:
+  //! * index is on main chain and higher than `height`.
+  //! * index is a fork and it's origin is higher than `height`.
+  virtual void LoadStatesHigherThan(
+      blockchain::Height height,
+      const esperanza::FinalizationParams &fin_params,
+      const esperanza::AdminParams &admin_params,
+      std::map<const CBlockIndex *, FinalizationState> *states) const = 0;
+
+  virtual ~StateDB() = default;
+
+  static std::unique_ptr<StateDB> New(
+      Dependency<Settings>,
+      Dependency<staking::BlockIndexMap>,
+      Dependency<staking::ActiveChain>);
+  static std::unique_ptr<StateDB> NewFromParams(
+      const StateDBParams &,
+      Dependency<Settings>,
+      Dependency<staking::BlockIndexMap>,
+      Dependency<staking::ActiveChain>);
+};
+
+}  // namespace finalization
+
+#endif

src/injector.h
@@ -10,11 +10,13 @@
 #include <blockchain/blockchain_behavior.h>
 #include <blockchain/blockchain_parameters.h>
 #include <blockchain/blockchain_rpc.h>
+#include <finalization/state_db.h>
 #include <finalization/state_processor.h>
 #include <finalization/state_repository.h>
 #include <p2p/finalizer_commits_handler.h>
 #include <settings.h>
 #include <staking/active_chain.h>
+#include <staking/block_index_map.h>
 #include <staking/block_validator.h>
 #include <staking/network.h>
 #include <staking/stake_validator.h>
@@ -45,6 +47,8 @@ class UnitEInjector : public Injector<UnitEInjector> {
 
   COMPONENT(Network, staking::Network, staking::Network::New)
 
+  COMPONENT(BlockIndexMap, staking::BlockIndexMap, staking::BlockIndexMap::New)
+
   COMPONENT(ActiveChain, staking::ActiveChain, staking::ActiveChain::New)
 
   COMPONENT(StakeValidator, staking::StakeValidator, staking::StakeValidator::New,
@@ -56,8 +60,11 @@ class UnitEInjector : public Injector<UnitEInjector> {
 
   COMPONENT(BlockDB, ::BlockDB, BlockDB::New)
 
+  COMPONENT(FinalizationStateDB, finalization::StateDB, finalization::StateDB::New,
+            Settings, staking::BlockIndexMap, staking::ActiveChain)
+
   COMPONENT(FinalizationStateRepository, finalization::StateRepository, finalization::StateRepository::New,
-            staking::ActiveChain)
+            staking::BlockIndexMap, staking::ActiveChain, finalization::StateDB, BlockDB)
 
   COMPONENT(FinalizationStateProcessor, finalization::StateProcessor, finalization::StateProcessor::New,
             finalization::StateRepository,

src/Makefile.test.include
@@ -67,6 +67,8 @@ UNITE_TESTS = \
   test/esperanza/finalizationstate_slash_tests.cpp \
   test/esperanza/finalizationstate_utils.h \
   test/esperanza/finalizationstate_utils.cpp \
+  test/finalization/params_tests.cpp \
+  test/finalization/state_db_tests.cpp \
   test/finalization/state_processor_tests.cpp \
   test/finalization/state_repository_tests.cpp \
   test/finalization/vote_recorder_tests.cpp \

src/blockchain/blockchain_parameters.cpp
@@ -93,6 +93,7 @@ Parameters Parameters::TestNet() noexcept {
   p.genesis_block = GenesisBlock(GenesisBlockBuilder().Add(TestnetFunds()).Build(p));
 
   p.default_settings.p2p_port = 17182;
+  p.data_dir_suffix = "testnet3";
 
   return p;
 }
@@ -123,6 +124,7 @@ Parameters Parameters::RegTest() noexcept {
   p.default_settings.node_is_proposer = false;
   p.default_settings.stake_split_threshold = 1000 * UNIT;
   p.default_settings.p2p_port = 17292;
+  p.data_dir_suffix = "regtest";
 
   return p;
 }

src/blockchain/blockchain_parameters.h
@@ -223,6 +223,9 @@ struct Parameters {
   //! \brief Number of blocks which have to have a softfork activated in a confirmation period.
   std::uint32_t rule_change_activation_threshold;
 
+  //! \brief Suffix of the data dir. In the path "~/user/.unite/regtest", it's a "regtest" suffix.
+  std::string data_dir_suffix;
+
   //! \brief Default settings to use for this chain.
   Settings default_settings;
 

src/esperanza/adminstate.cpp
@@ -3,6 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <esperanza/adminstate.h>
+#include <util.h>
 
 namespace esperanza {
 
@@ -59,6 +60,17 @@ bool AdminState::operator==(const AdminState &other) const {
   return m_admin_pub_keys == other.m_admin_pub_keys &&
          m_white_list == other.m_white_list &&
          m_permissioning_is_active == other.m_permissioning_is_active;
+}
+
+std::string AdminState::ToString() const {
+  return strprintf(
+      "AdminState{"
+      "m_admin_pub_keys=%s "
+      "m_white_list=%s "
+      "m_permissioning_is_active=%d}",
+      util::to_string(m_admin_pub_keys),
+      util::to_string(m_white_list),
+      m_permissioning_is_active);
 }
 
 }  // namespace esperanza

commit db2dbc9090c8d8a1a33f109a7505c87f8160df32

Allow unit tests being run in parallel again (#870)
Currently unit tests can not be run in parallel. This prevents us from speeding up ci builds (see https://github.com/bitcoin/bitcoin/pull/12926, https://github.com/dtr-org/unit-e/pull/865) and won't work with the updated build definitions from bitcoin 0.17 (#860). This is a regression which was introduced in https://github.com/dtr-org/unit-e/pull/793. This pull request fixes https://github.com/dtr-org/unit-e/issues/861.

The problem is that the `StateDB` component does something actively when being initialized (not something a component should do), which is to access disk and so on, which can fail. Since there is a `UnitEInjector` for every `BasicTestingSetup` running the unit tests in parallel creates a database _in the same directory_ per unit tests suite which clashes and breaks and throws and aborts and dies. This can be prevented by using an in-memory database, but the problem shifts: How to get that configuration in?

I did not want to expose this as a user-definable setting and also not as a chain parameter, so these two configuration options are not available. Which is why I created `UnitEInjectorConfiguration`. This is something which should not be necessary as usually you would not expose the same module as in production in unit tests but use a subset or just the mocked version, but since the rest of bitcoin is not in well-defined components and accesses them using `GetComponent`  a global injector has to be available in some tests.

The change ultimately does not affect the prod version of things, but the tests, which inject a different `UnitEInjectorConfiguration`. I made this a `struct` rather then just a flag `unit_tests` such that it is extensible in case we have other cases like this. In order to make the injector be able to access fields from it's own instance in the definition of a component I altered the definition of `UNMANAGED_COMPONENT` a bit.

src/Makefile.am
@@ -142,6 +142,7 @@ UNITE_CORE_H = \
   indirectmap.h \
   init.h \
   injector.h \
+  injector_config.h \
   key.h \
   key/mnemonic/mnemonic.h \
   key/mnemonic/chinese_simplified.h \

src/dependency_injector.h
@@ -128,13 +128,13 @@ struct Invoker<> {
   }();                                                                          \
   Dependency<TYPE> Get(TYPE *) const { return m_component_##NAME; }
 
-#define UNMANAGED_COMPONENT(NAME, TYPE, POINTER)                     \
+#define UNMANAGED_COMPONENT(NAME, TYPE, FACTORY)                     \
  private:                                                            \
   Dependency<TYPE> m_component_##NAME = [this] {                     \
     return Registrator<TYPE>::Register<>(                            \
         this, #NAME, [](InjectorType *injector) -> void {            \
           injector->m_component_##NAME =                             \
-              Initializer<TYPE>::Unmanaged::Init(injector, POINTER); \
+              Initializer<TYPE>::Unmanaged::Init(injector, FACTORY); \
         });                                                          \
   }();                                                               \
   Dependency<TYPE> Get(TYPE *) const { return m_component_##NAME; }
@@ -231,10 +231,13 @@ class Injector {
   using InjectorType = I;
 
   // a function pointer to a function that takes an Injector as its first
-  // argument. Used for static methos to act as if they were non-static member
+  // argument. Used for static methods to act as if they were non-static member
   // methods, but we need a stable function pointer to them (thus static) and
   // they need access to the injector (hence it's passed in).
   using Method = void (*)(InjectorType *);
+
+  template <typename T>
+  using Factory = T *(*)(InjectorType *);
 
   struct Component {
     std::string m_name;
@@ -321,17 +324,16 @@ class Injector {
         auto &component = GetComponent(injector);
         const auto dependencies = GatherDependencies(injector, component);
         std::unique_ptr<ComponentType> *returnTypeDeductionHint = nullptr;
-        auto ptr = InjectorUtil::Invoker<Args...>::
-                       Invoke(returnTypeDeductionHint, f, dependencies, 0)
-                           .release();
+        auto ptr = InjectorUtil::Invoker<Args...>::Invoke(returnTypeDeductionHint, f, dependencies, 0).release();
         component.m_instance = ptr;
         return ptr;
       }
     };
     struct Unmanaged {
       template <typename T>
-      static ComponentType *Init(I *injector, T *ptr) {
+      static ComponentType *Init(I *injector, Factory<T> factory) {
         auto &component = GetComponent(injector);
+        T *const ptr = factory(injector);
         component.m_instance = ptr;
         component.m_deleter = nullptr;
         return ptr;

src/finalization/state_db.cpp
@@ -6,6 +6,7 @@
 
 #include <dbwrapper.h>
 #include <esperanza/finalizationstate.h>
+#include <injector_config.h>
 #include <staking/active_chain.h>
 #include <staking/block_index_map.h>
 #include <validation.h>
@@ -162,10 +163,13 @@ void StateDBImpl::LoadStatesHigherThan(
 }  // namespace
 
 std::unique_ptr<StateDB> StateDB::New(
+    Dependency<UnitEInjectorConfiguration> config,
     Dependency<Settings> settings,
     Dependency<staking::BlockIndexMap> block_index_map,
     Dependency<staking::ActiveChain> active_chain) {
-  return NewFromParams(StateDBParams{}, settings, block_index_map, active_chain);
+  StateDBParams state_db_params;
+  state_db_params.inmemory = config->use_in_memory_databases;
+  return NewFromParams(state_db_params, settings, block_index_map, active_chain);
 }
 
 std::unique_ptr<StateDB> StateDB::NewFromParams(

src/finalization/state_db.h
@@ -21,6 +21,7 @@ class BlockIndexMap;
 }  // namespace staking
 
 class CBlockIndex;
+class UnitEInjectorConfiguration;
 
 namespace finalization {
 
@@ -68,9 +69,11 @@ class StateDB {
   virtual ~StateDB() = default;
 
   static std::unique_ptr<StateDB> New(
+      Dependency<UnitEInjectorConfiguration>,
       Dependency<Settings>,
       Dependency<staking::BlockIndexMap>,
       Dependency<staking::ActiveChain>);
+
   static std::unique_ptr<StateDB> NewFromParams(
       const StateDBParams &,
       Dependency<Settings>,

src/injector.cpp
@@ -8,9 +8,9 @@ namespace {
 std::unique_ptr<UnitEInjector> injector = nullptr;
 }
 
-void UnitEInjector::Init() {
+void UnitEInjector::Init(UnitEInjectorConfiguration config) {
   assert(!injector);
-  injector = MakeUnique<UnitEInjector>();
+  injector = MakeUnique<UnitEInjector>(config);
   injector->Initialize();
 }
 

src/injector.h
@@ -10,9 +10,11 @@
 #include <blockchain/blockchain_behavior.h>
 #include <blockchain/blockchain_parameters.h>
 #include <blockchain/blockchain_rpc.h>
+#include <blockdb.h>
 #include <finalization/state_db.h>
 #include <finalization/state_processor.h>
 #include <finalization/state_repository.h>
+#include <injector_config.h>
 #include <p2p/finalizer_commits_handler.h>
 #include <settings.h>
 #include <staking/active_chain.h>
@@ -21,7 +23,6 @@
 #include <staking/network.h>
 #include <staking/stake_validator.h>
 #include <staking/transactionpicker.h>
-#include <blockdb.h>
 #include <util.h>
 #include <validation.h>
 
@@ -34,9 +35,14 @@
 
 class UnitEInjector : public Injector<UnitEInjector> {
 
-  UNMANAGED_COMPONENT(ArgsManager, ::ArgsManager, &gArgs)
+  static ::ArgsManager *GetGlobalArgs(UnitEInjector *) { return &gArgs; }
+  UNMANAGED_COMPONENT(ArgsManager, ::ArgsManager, GetGlobalArgs)
 
-  UNMANAGED_COMPONENT(BlockchainBehavior, blockchain::Behavior, &blockchain::Behavior::GetGlobal())
+  static blockchain::Behavior *GetGlobalBehavior(UnitEInjector *) { return &blockchain::Behavior::GetGlobal(); }
+  UNMANAGED_COMPONENT(BlockchainBehavior, blockchain::Behavior, GetGlobalBehavior)
+
+  static UnitEInjectorConfiguration *GetInjectorConfiguration(UnitEInjector *injector) { return &injector->m_config; }
+  UNMANAGED_COMPONENT(InjectorConfiguration, UnitEInjectorConfiguration, GetInjectorConfiguration)
 
   COMPONENT(Settings, ::Settings, Settings::New,
             ::ArgsManager,
@@ -61,10 +67,16 @@ class UnitEInjector : public Injector<UnitEInjector> {
   COMPONENT(BlockDB, ::BlockDB, BlockDB::New)
 
   COMPONENT(FinalizationStateDB, finalization::StateDB, finalization::StateDB::New,
-            Settings, staking::BlockIndexMap, staking::ActiveChain)
+            UnitEInjectorConfiguration,
+            Settings,
+            staking::BlockIndexMap,
+            staking::ActiveChain)
 
   COMPONENT(FinalizationStateRepository, finalization::StateRepository, finalization::StateRepository::New,
-            staking::BlockIndexMap, staking::ActiveChain, finalization::StateDB, BlockDB)
+            staking::BlockIndexMap,
+            staking::ActiveChain,
+            finalization::StateDB,
+            BlockDB)
 
   COMPONENT(FinalizationStateProcessor, finalization::StateProcessor, finalization::StateProcessor::New,
             finalization::StateRepository,
@@ -109,9 +121,13 @@ class UnitEInjector : public Injector<UnitEInjector> {
 
 #endif
 
+  UnitEInjectorConfiguration m_config;
+
  public:
+  explicit UnitEInjector(UnitEInjectorConfiguration config) : m_config(config) {}
+
   //! \brief Initializes a globally available instance of the injector.
-  static void Init();
+  static void Init(UnitEInjectorConfiguration = UnitEInjectorConfiguration{});
 
   //! \brief Destructs the injector and all components managed by it.
   static void Destroy();

src/injector_config.h
@@ -1,0 +1,12 @@
+// Copyright (c) 2019 The Unit-e developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef UNIT_E_INJECTOR_CONFIG_H
+#define UNIT_E_INJECTOR_CONFIG_H
+
+struct UnitEInjectorConfiguration {
+  bool use_in_memory_databases = false;
+};
+
+#endif //UNIT_E_INJECTOR_CONFIG_H

src/test/dependency_injector_tests.cpp
@@ -185,7 +185,8 @@ class Inj : public Injector<Inj> {
 };
 
 class UnmanagedInj : public Injector<UnmanagedInj> {
-  UNMANAGED_COMPONENT(One, ComplexThing, global_ptr.get())
+  static ComplexThing *GetGlobalPtr(UnmanagedInj *) { return global_ptr.get(); }
+  UNMANAGED_COMPONENT(One, ComplexThing, GetGlobalPtr)
   COMPONENT(Two, InjTestNS::Q, InjTestNS::Q::Make, ComplexThing)
  public:
   template <typename T>

src/test/injector_tests.cpp
@@ -11,13 +11,19 @@
 
 BOOST_AUTO_TEST_SUITE(injector_tests)
 
+static const UnitEInjectorConfiguration config = [](){
+  UnitEInjectorConfiguration cfg;
+  cfg.use_in_memory_databases = true;
+  return cfg;
+}();
+
 BOOST_AUTO_TEST_CASE(check_order) {
-  UnitEInjector injector;
+  UnitEInjector injector(config);
   BOOST_CHECK_NO_THROW(injector.DetermineInitializationOrder());
 }
 
 BOOST_AUTO_TEST_CASE(try_initialize_mainnet) {
-  UnitEInjector injector;
+  UnitEInjector injector(config);
   ArgsManager args;
   const char *const argv[] = {
       "./united"};
@@ -27,7 +33,7 @@ BOOST_AUTO_TEST_CASE(try_initialize_mainnet) {
 }
 
 BOOST_AUTO_TEST_CASE(try_initialize_testnet) {
-  UnitEInjector injector;
+  UnitEInjector injector(config);
   ArgsManager args;
   const char *const argv[] = {
       "./united",
@@ -38,7 +44,7 @@ BOOST_AUTO_TEST_CASE(try_initialize_testnet) {
 }
 
 BOOST_AUTO_TEST_CASE(try_initialize_regtest) {
-  UnitEInjector injector;
+  UnitEInjector injector(config);
   ArgsManager args;
   const char *const argv[] = {
       "./united",

src/test/test_unite.cpp
@@ -82,7 +82,9 @@ ReducedTestingSetup::~ReducedTestingSetup()
 BasicTestingSetup::BasicTestingSetup(const std::string& chainName) : ReducedTestingSetup(chainName)
 {
         blockchain::Behavior::SetGlobal(blockchain::Behavior::NewForNetwork(blockchain::Network::_from_string(chainName.c_str())));
-        UnitEInjector::Init();
+        UnitEInjectorConfiguration config;
+        config.use_in_memory_databases = true;
+        UnitEInjector::Init(config);
         SelectParams(GetComponent<blockchain::Behavior>(), chainName);
 }
 