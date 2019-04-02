e20483954776e497ddd1b86eb6ad6b046a8a11c3
Aleksandr Mikhailov <aleksandr@thirdhash.com>
2019-04-02 10:57:30 +0200

    Run only investigated test 5 times
    Signed-off-by: Aleksandr Mikhailov <aleksandr@thirdhash.com>

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -335,6 +335,7 @@ def main():
     run_tests(test_list, config["environment"]["SRCDIR"], config["environment"]["BUILDDIR"], config["environment"]["EXEEXT"], tmpdir, args.jobs, args.coverage, passon_args, args.combinedlogslen)
 
 def run_tests(test_list, src_dir, build_dir, exeext, tmpdir, jobs=1, enable_coverage=False, args=[], combined_logs_len=0):
+    test_list = ['feature_no_esperanza_tx_relay_delay.py'] * 5
     # Warn if united is already running (unix only)
     try:
         if subprocess.check_output(["pidof", "united"]) is not None:
