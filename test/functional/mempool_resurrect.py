7f03a48dead7b5b7657e5e5822f843831d7ceea5
Julian Fleischer <julian@thirdhash.com>
2019-04-02 18:50:22 +0200

    Fix mempool_resurrect
    Signed-off-by: Julian Fleischer <julian@thirdhash.com>

--- test/functional/mempool_resurrect.py
+++ test/functional/mempool_resurrect.py
@@ -4,6 +4,8 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Test resurrection of mined transactions when the blockchain is re-organized."""
 
+from decimal import Decimal
+
 from test_framework.blocktools import create_raw_transaction
 from test_framework.test_framework import UnitETestFramework, PROPOSER_REWARD
 from test_framework.util import assert_equal
