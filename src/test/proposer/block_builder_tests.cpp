03ad30a6bf8c483467347e3485e3fdab6af2d702
Julian Fleischer <scravy@users.noreply.github.com>
2019-04-04 13:17:02 +0200

    Use ArgsManagerMock (#890)
    A small refactoring which allows to use an `ArgsManager` initialized with some arguments in all tests.
    
    Signed-off-by: Julian Fleischer <julian@thirdhash.com>

--- src/test/proposer/block_builder_tests.cpp
+++ src/test/proposer/block_builder_tests.cpp
@@ -12,6 +12,7 @@
 #include <test/test_unite.h>
 #include <boost/test/unit_test.hpp>
 
+#include <test/test_unite_mocks.h>
 #include <algorithm>
 #include <cstdlib>
 
@@ -66,18 +67,7 @@ struct Fixture {
   std::vector<unsigned char> pubkeydata;
 
   Fixture(std::initializer_list<std::string> args)
-      : args_manager([&] {
-          std::unique_ptr<::ArgsManager> argsman = MakeUnique<::ArgsManager>();
-          const char **argv = new const char *[args.size() + 1];
-          argv[0] = "executable-name";
-          std::size_t i = 1;
-          for (const auto &arg : args) {
-            argv[i++] = arg.c_str();
-          }
-          argsman->ParseParameters(static_cast<int>(i), argv);
-          delete[] argv;
-          return argsman;
-        }()),
+      : args_manager(new mocks::ArgsManagerMock(args)),
         settings(Settings::New(args_manager.get(), behavior.get())) {
 
     const key::mnemonic::Seed seed("scout wheat rhythm inmate make insect chimney interest fire oxygen gap party slush grid post");
