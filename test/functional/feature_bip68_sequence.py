736f3ebd26a36cdc053026284397f7997bf2576d
Andres Correa Casablanca <andres@thirdhash.com>
2019-04-08 17:42:48 +0200

    Remove outdated comment
    Signed-off-by: Andres Correa Casablanca <andres@thirdhash.com>

--- test/functional/feature_bip68_sequence.py
+++ test/functional/feature_bip68_sequence.py
@@ -336,7 +336,7 @@ class BIP68Test(UnitETestFramework):
             tip_snapshot_meta = update_snapshot_with_tx(self.nodes[0], tip_snapshot_meta.data, 0, height, coinbase)
 
             height += 1
-            self.nodes[0].p2p.send_and_ping(msg_witness_block(block))  # It crashes HERE
+            self.nodes[0].p2p.send_and_ping(msg_witness_block(block))
             cur_time += 1
 
         # sync as the reorg is happening
