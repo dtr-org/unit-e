96a4d39b7febb0a616800cd01fd5ee5f9268aba9
Kostiantyn Stepaniuk <kostia@thirdhash.com>
2019-04-11 12:24:34 +0200

    Run 30 times on travis
    Signed-off-by: Kostiantyn Stepaniuk <kostia@thirdhash.com>

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -343,6 +343,7 @@ def main():
     if not args.keepcache:
         shutil.rmtree("%s/test/cache" % config["environment"]["BUILDDIR"], ignore_errors=True)
 
+    test_list = ['esperanza_withdraw.py'] * 30
     run_tests(test_list, config["environment"]["SRCDIR"], config["environment"]["BUILDDIR"], config["environment"]["EXEEXT"], tmpdir, args.jobs, args.coverage, passon_args, args.combinedlogslen)
 
 def run_tests(test_list, src_dir, build_dir, exeext, tmpdir, jobs=1, enable_coverage=False, args=[], combined_logs_len=0):
