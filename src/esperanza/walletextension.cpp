576a1ff1e216979558dfb1ea997b1bcebea2005d
Dmitry Saveliev <dima@thirdhash.com>
2019-04-11 18:03:50 +0200

    Fix ForEachStakeableCoin - check containing block
    Signed-off-by: Dmitry Saveliev <dima@thirdhash.com>

--- src/esperanza/walletextension.cpp
+++ src/esperanza/walletextension.cpp
@@ -57,11 +57,11 @@ void WalletExtension::ForEachStakeableCoin(Callable f) const {
     const CWalletTx *const tx = &it.second;
     const uint256 &txId = tx->GetHash();
     const std::vector<::CTxOut> &coins = tx->tx->vout;
-    const CBlockIndex *containing_block;
-    tx->GetDepthInMainChain(containing_block);  // requires cs_main
-    if (!containing_block) {
-      // transaction is not included in a block
-      continue;
+    const CBlockIndex *containing_block = nullptr;
+    const int depth = tx->GetDepthInMainChain(containing_block);  // requires cs_main
+    if (depth == 0 || !containing_block) {
+        // transaction is not included in a block
+        continue;
     }
 
     blockchain::Height height = static_cast<blockchain::Height>(containing_block->nHeight);
