eaa5a3efb8570e39cde998bf6fa5de80d0ed493a
Julian Fleischer <julian@thirdhash.com>
2019-04-12 11:20:27 +0200

    Small propopser cleanups
    Signed-off-by: Julian Fleischer <julian@thirdhash.com>

--- src/proposer/proposer_state.h
+++ src/proposer/proposer_state.h
@@ -16,11 +16,17 @@ struct State {
 
   Status m_status = Status::NOT_PROPOSING;
 
-  //! how many search cycles the proposer went through
+  //! \brief how often the proposer looked for an eligible coin
   std::uint64_t m_number_of_searches = 0;
 
-  //! how many search cycles the proposer attempted
+  //! \brief how often the proposer found an eligible coin
   std::uint64_t m_number_of_search_attempts = 0;
+
+  //! \brief how often the proposer actually proposed
+  std::uint64_t m_number_of_proposed_blocks = 0;
+
+  //! \brief how many transactions the proposer included in proposed blocks in total
+  std::uint64_t m_number_of_transactions_included = 0;
 };
 
 }  // namespace proposer
