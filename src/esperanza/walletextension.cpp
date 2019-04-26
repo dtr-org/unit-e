4a06165ca03afb026edf8675dfc0251930e4e67d
Matteo Sumberaz <matteo@thirdhash.com>
2019-04-26 16:01:29 +0200

    Remove coin combining from generate
    Signed-off-by: Matteo Sumberaz <matteo@thirdhash.com>

--- src/esperanza/walletextension.cpp
+++ src/esperanza/walletextension.cpp
@@ -111,6 +111,11 @@ staking::CoinSet WalletExtension::GetStakeableCoins() const {
     COutPoint out_point(tx->tx->GetHash(), out_index);
     coins.emplace(containing_block, out_point, tx->tx->vout[out_index]);
   });
+
+  std::cout << "stakeable coins are: " << std::endl;
+  for (auto coin : coins) {
+    std::cout << coin.ToString() << std::endl;
+  }
   return coins;
 }
 
--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -3468,7 +3468,7 @@ UniValue GenerateBlocks(CWallet *const wallet,
 
             if (!proposer->GenerateBlock(wallet,
                                          *active_chain->GetTip(),
-                                         coins,
+                                         staking::CoinSet(),
                                          coinbase_script.get()->reserveScript,
                                          block)) {
                 throw JSONRPCError(RPC_INTERNAL_ERROR, "Failed to generate a block.");
