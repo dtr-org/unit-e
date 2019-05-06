adaf162955a925b43e71132d6c90ebd0fce77559
Matteo Sumberaz <matteo@thirdhash.com>
2019-05-06 08:32:38 +0200

    Add params to dispatch table
    Signed-off-by: Matteo Sumberaz <matteo@thirdhash.com>

--- src/rpc/proposing.cpp
+++ src/rpc/proposing.cpp
@@ -19,8 +19,8 @@
   t.appendCommand(NAME.name, &NAME);
 
 void RegisterProposerRPCCommands(CRPCTable &t) {
-  PROPOSER_RPC_COMMAND(propose);
-  PROPOSER_RPC_COMMAND(proposetoaddress);
+  PROPOSER_RPC_COMMAND(propose, "nblocks");
+  PROPOSER_RPC_COMMAND(proposetoaddress, "nblocks", "address");
   PROPOSER_RPC_COMMAND(liststakeablecoins);
   PROPOSER_RPC_COMMAND(proposerstatus);
   PROPOSER_RPC_COMMAND(proposerwake);
