4f785018c96f358905d665007791a405975c3b32
Cornelius Schumacher <cornelius@thirdhash.com>
2019-05-07 12:07:43 +0200

    Run unit tests of Python scripts in contrib/
    There are a couple of scripts in contrib/ which have unit tests. These
    aren't run by CI yet. This change adds a step to the linter script
    which runs the unit tests before any scripts are run.
    
    Signed-off-by: Cornelius Schumacher <cornelius@thirdhash.com>

--- .travis/lint_06_script.sh
+++ .travis/lint_06_script.sh
@@ -6,6 +6,8 @@
 
 export LC_ALL=C
 
+test/lint/run-python-tests.sh
+
 if [ "$TRAVIS_EVENT_TYPE" = "pull_request" ]; then
   test/lint/commit-script-check.sh $TRAVIS_COMMIT_RANGE
 fi
---
+++ test/lint/run-python-tests.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2018 The Unit-e developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#
+# Run unit tests for Python scripts in contrib/
+
+python3 contrib/devtools/test_copyright_header.py
+python3 contrib/devtools/test_camel_to_snake.py
