48d45fecc16bf285ded83e36fccd1ac795e84fd2
Cornelius Schumacher <cornelius@thirdhash.com>
2019-05-07 13:12:21 +0200

    Exit with error when one of the tests fails
    Signed-off-by: Cornelius Schumacher <cornelius@thirdhash.com>

--- test/lint/run-python-tests.sh
+++ test/lint/run-python-tests.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
 #
-# Copyright (c) 2018 The Unit-e developers
+# Copyright (c) 2019 The Unit-e developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #
 # Run unit tests for Python scripts in contrib/
 
-python3 contrib/devtools/test_copyright_header.py
-python3 contrib/devtools/test_camel_to_snake.py
+failed=false
+
+run_unit_test() {
+  echo "Running unit test $1..."
+  if ! python3 $1; then
+    failed=true
+  fi
+}
+
+run_unit_test contrib/devtools/test_copyright_header.py
+run_unit_test contrib/devtools/test_camel_to_snake.py
+
+if $failed; then
+  echo run-python-tests FAILED
+  exit 1
+else
+  echo run-python-tests PASSED
+fi
