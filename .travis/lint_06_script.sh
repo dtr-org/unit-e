4720b0f613c8b577cfbe6672aef80409861bb912
Cornelius Schumacher <cornelius@thirdhash.com>
2019-05-08 16:49:40 +0200

    Run all tests with pytest
    Signed-off-by: Cornelius Schumacher <cornelius@thirdhash.com>

--- .travis/lint_06_script.sh
+++ .travis/lint_06_script.sh
@@ -6,7 +6,7 @@
 
 export LC_ALL=C
 
-test/lint/run-python-tests.sh
+(cd contrib; pytest)
 
 if [ "$TRAVIS_EVENT_TYPE" = "pull_request" ]; then
   test/lint/commit-script-check.sh $TRAVIS_COMMIT_RANGE
