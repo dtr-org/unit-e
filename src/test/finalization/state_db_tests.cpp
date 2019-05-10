fabef31f9727d6dfb53b58d0a500614507e3881f
Julian Fleischer <julian@thirdhash.com>
2019-05-10 15:04:19 +0200

    Rename it to kv
    Signed-off-by: Julian Fleischer <julian@thirdhash.com>

--- src/test/finalization/state_db_tests.cpp
+++ src/test/finalization/state_db_tests.cpp
@@ -173,9 +173,9 @@ BOOST_AUTO_TEST_CASE(load_best_states) {
     db->LoadStatesHigherThan(59, &restored);
     BOOST_CHECK_EQUAL(restored.size(), 140);
 
-    for (auto &it : restored) {
-      const CBlockIndex *index = it.first;
-      const esperanza::FinalizationState &state = it.second;
+    for (auto &kv : restored) {
+      const CBlockIndex *index = kv.first;
+      const esperanza::FinalizationState &state = kv.second;
       BOOST_CHECK(index->nHeight >= 60);
 
       const auto oit = original.find(index);
